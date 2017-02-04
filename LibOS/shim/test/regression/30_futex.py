11029ffff821be1d5836b90fa159f2ef56325531
Don Porter <porter@cs.unc.edu>
2017-02-04 16:39:01 -0500

    Futex and clone fixes. New futex unit test. May help issue #40.

---
+++ LibOS/shim/test/regression/30_futex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os, sys, mmap
+from regression import Regression
+
+loader = sys.argv[1]
+
+# Running futex
+regression = Regression(loader, "futex")
+
+regression.add_check(name="Futex Wake Test",
+    check=lambda res: "Woke all kiddos" in res[0].out)
+
+regression.run_checks()
