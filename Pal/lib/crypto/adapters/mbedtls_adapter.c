5549fca4b70451cdb873f943cfe4b753e1bcf373
Don Porter <porter@cs.unc.edu>
2017-09-23 09:08:53 -0400

    Another merge error

--- Pal/lib/crypto/adapters/mbedtls_adapter.c
+++ Pal/lib/crypto/adapters/mbedtls_adapter.c
@@ -1,4 +1,6 @@
-/* This file is part of Graphene Library OS.
+/* Copyright (C) 2017 Fortanix, Inc.
+
+   This file is part of Graphene Library OS.
 
    Graphene Library OS is free software: you can redistribute it and/or
    modify it under the terms of the GNU General Public License
