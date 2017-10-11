423050dc4b8dfa89e274292647588a744067edf3
Chia-Che Tsai <chitsai@cs.stonybrook.edu>
2017-10-11 14:36:11 -0400

    git rid of unnecessary dependencies

--- Pal/lib/crypto/adapters/mbedtls_dh.c
+++ Pal/lib/crypto/adapters/mbedtls_dh.c
@@ -15,8 +15,6 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-#include <errno.h>
-#include <string.h>
 #include "pal.h"
 #include "pal_crypto.h"
 #include "pal_error.h"
