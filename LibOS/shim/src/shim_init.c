efef98a5cf6abe460ceb697a423c5dcaf4117f1c
Chia-Che Tsai <chitsai@cs.stonybrook.edu>
2017-12-12 13:55:07 -0500

    revert an accidential change

--- LibOS/shim/src/shim_init.c
+++ LibOS/shim/src/shim_init.c
@@ -767,7 +767,7 @@ restore:
     void * stack_top = NULL;
     RUN_INIT(init_stack, argv, envp, &argp, &nauxv, &auxp, &stack_top);
     RUN_INIT(init_loader);
-    //RUN_INIT(init_ipc_helper);
+    RUN_INIT(init_ipc_helper);
     RUN_INIT(init_signal);
 
     debug("shim process initialized\n");
