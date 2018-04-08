e23f0e6eb6f48aaac3ac6acc3f96c712fc9b9678
Chia-Che Tsai <chitsai@cs.stonybrook.edu>
2018-04-08 07:51:27 -0700

    Handle brk initialization failure

--- LibOS/shim/src/sys/shim_brk.c
+++ LibOS/shim/src/sys/shim_brk.c
@@ -117,10 +117,11 @@ int init_brk_region (void * brk_region)
     void * end_brk_region = NULL;
 
     /* Allocate the whole brk region */
-    brk_region = (void *) DkVirtualMemoryAlloc(brk_region, brk_max_size, 0,
-                                    PAL_PROT_READ|PAL_PROT_WRITE);
+    void * ret = (void *) DkVirtualMemoryAlloc(brk_region, brk_max_size, 0,
+                                               PAL_PROT_READ|PAL_PROT_WRITE);
 
-    if (!brk_region) {
+    /* Checking if the PAL call succeeds. */
+    if (!ret) {
         bkeep_munmap(brk_region, brk_max_size, flags);
         return -ENOMEM;
     }
@@ -176,7 +177,12 @@ int reset_brk (void)
 void * shim_do_brk (void * brk)
 {
     master_lock();
-    init_brk_region(NULL);
+
+    if (init_brk_region(NULL) < 0) {
+        debug("Failed to initialize brk!\n");
+        brk = NULL;
+        goto out;
+    }
 
     if (!brk) {
 unchanged:
