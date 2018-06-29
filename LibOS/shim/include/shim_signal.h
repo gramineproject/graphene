aede6b6af805f82a8c600aeee794f2a186027ac3
Don Porter <porter@cs.unc.edu>
2018-06-29 13:57:18 -0400

    Correct definition of bits per word, as reported in #196. (#217)

--- LibOS/shim/include/shim_signal.h
+++ LibOS/shim/include/shim_signal.h
@@ -13,10 +13,7 @@ struct shim_signal_handle {
     struct __kernel_sigaction * action;
 };
 
-#define NUM_SIGS            64
-#define NUM_KNOWN_SIGS      32
-
-# define BITS_PER_WORD sizeof(unsigned long)
+# define BITS_PER_WORD (8 * sizeof(unsigned long))
 /* The standard def of this macro is dumb */
 #undef _SIGSET_NWORDS
 # define _SIGSET_NWORDS (NUM_SIGS / BITS_PER_WORD)
