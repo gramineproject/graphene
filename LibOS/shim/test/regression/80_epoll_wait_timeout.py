8d0b41b9f706d471683e63545a9e7179a929d41d
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2018-10-09 14:19:51 -0700

    Fixes oscarlab/graphene#239 (shim_do_epoll_wait ignores user-supplied timeout)

---
+++ LibOS/shim/test/regression/80_epoll_wait_timeout.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os, sys, mmap
+from regression import Regression
+
+loader = sys.argv[1]
+
+regression = Regression(loader, "epoll_wait_timeout", None, 50000)
+
+regression.add_check(name="epoll_wait timeout",
+    check=lambda res: "epoll_wait test passed" in res[0].out)
+
+rv = regression.run_checks()
+if rv: sys.exit(rv)
