25bd21e228750d5d7a307e7264f7bc5a84d3d884
Chia-Che Tsai <chitsai@cs.stonybrook.edu>
2018-11-03 14:31:36 -0700

    [Linux PAL] Rewrite signal handling

--- Pal/src/host/Linux/db_main.c
+++ Pal/src/host/Linux/db_main.c
@@ -229,6 +229,26 @@ void pal_linux_main (void * args)
 
     init_slab_mgr(pagesz);
 
+    first_thread = malloc(HANDLE_SIZE(thread));
+    if (!first_thread)
+        init_fail(PAL_ERROR_NOMEM, "Out of memory");
+    SET_HANDLE_TYPE(first_thread, thread);
+    first_thread->thread.tid = INLINE_SYSCALL(gettid, 0);
+
+    void * alt_stack = NULL;
+    _DkVirtualMemoryAlloc(&alt_stack, ALT_STACK_SIZE, 0, PAL_PROT_READ|PAL_PROT_WRITE);
+    if (!alt_stack)
+        init_fail(PAL_ERROR_NOMEM, "Out of memory");
+
+    // Initialize TCB at the top of the alternative stack.
+    PAL_TCB * tcb  = alt_stack + ALT_STACK_SIZE - sizeof(PAL_TCB);
+    tcb->self      = tcb;
+    tcb->handle    = first_thread;
+    tcb->alt_stack = alt_stack; // Stack bottom
+    tcb->callback  = NULL;
+    tcb->param     = NULL;
+    pal_thread_init(tcb);
+
     setup_pal_map(&pal_map);
 
 #if USE_VDSO_GETTIME == 1
@@ -285,10 +305,6 @@ done_init:
         _DkProcessExit(0);
         return;
     }
-
-    first_thread = malloc(HANDLE_SIZE(thread));
-    SET_HANDLE_TYPE(first_thread, thread);
-    first_thread->thread.tid = linux_state.pid;
 
     signal_setup();
 
