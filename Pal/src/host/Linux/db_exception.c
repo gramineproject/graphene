b5393910088c747623f36d004ee2fcf0565ebec8
Chia-Che Tsai <chitsai@cs.stonybrook.edu>
2018-11-05 18:40:04 -0800

    [Linux PAL] Fix a bug in DkObjectsWaitAny(); remove unused code in host/Linux/db_exception.c.

--- Pal/src/host/Linux/db_exception.c
+++ Pal/src/host/Linux/db_exception.c
@@ -164,28 +164,9 @@ static bool _DkGenericSignalHandle (int event_num, siginfo_t * info,
     return false;
 }
 
-#if BLOCK_SIGFAULT == 1
-static char exception_msg[24] = "--- SIGSEGV --- [     ]\n";
-static volatile bool cont_exec = false;
-#endif
-
 static void _DkGenericSighandler (int signum, siginfo_t * info,
                                   struct ucontext * uc)
 {
-#if BLOCK_SIGFUALT == 1
-    /* reseurrect this code if signal handler if giving segmentation fault */
-    if (signum == SIGSEGV) {
-        int pid = INLINE_SYSCALL(getpid, 0);
-        exception_msg[17] = '0' + pid / 10000;
-        exception_msg[18] = '0' + (pid / 1000) % 10;
-        exception_msg[19] = '0' + (pid / 100) % 10;
-        exception_msg[20] = '0' + (pid / 10) % 10;
-        exception_msg[21] = '0' + pid % 10;
-        INLINE_SYSCALL(write, 3, 1, exception_msg, 24);
-        while(!cont_exec);
-    }
-#endif
-
     int event_num = get_event_num(signum);
     if (event_num == -1)
         return;
