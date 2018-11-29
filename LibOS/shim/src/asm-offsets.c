79f5f10503fe00fb895fdecfea7804a8016652f4
Isaku Yamahata <isaku.yamahata@gmail.com>
2018-11-28 22:51:29 -0800

    [LibOS] asm offset constants generation
    Auto generate offset constants for assembly. replace magic number with
    symbolic constants in LibOS/shim/src/syscallas.S.
    
    This patch also introduces Makefile.rules to accommodate common make rules
    so that V=1 (like Linux style make) is accepted.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

---
+++ LibOS/shim/src/asm-offsets.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+
+#include <shim_internal.h>
+#include <shim_tls.h>
+
+#define OFFSET_T(name, str_t, member)                       \
+    asm volatile(".ascii \" #define " #name " %0 \"\n"::    \
+                 "i"(offsetof(str_t, member)))
+
+void dummy(void)
+{
+    OFFSET_T(SHIM_TCB_OFFSET, __libc_tcb_t, shim_tcb);
+    OFFSET_T(TCB_SYSCALL_NR, shim_tcb_t, context.syscall_nr);
+    OFFSET_T(TCB_SP, shim_tcb_t, context.sp);
+    OFFSET_T(TCB_RET_IP, shim_tcb_t, context.ret_ip);
+    OFFSET_T(TCB_REGS, shim_tcb_t, context.regs);
+}
+
