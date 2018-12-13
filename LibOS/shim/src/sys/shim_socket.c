32931d2eac31f97dcca5ec57f49ad4d27190966e
Thomas Knauth <thomas.knauth@intel.com>
2018-12-13 10:36:26 -0800

    Explicitly return error instead of silently ignoring parameter.

--- LibOS/shim/src/sys/shim_socket.c
+++ LibOS/shim/src/sys/shim_socket.c
@@ -1192,6 +1192,9 @@ int shim_do_sendmmsg (int sockfd, struct mmsghdr * msg, int vlen, int flags)
 static ssize_t do_recvmsg (int fd, struct iovec * bufs, int nbufs, int flags,
                            struct sockaddr * addr, socklen_t * addrlen)
 {
+    /* TODO handle flags properly. For now, just return an error. */
+    if (flags) return -EINVAL;
+    
     struct shim_handle * hdl = get_fd_handle(fd, NULL, NULL);
     if (!hdl)
         return -EBADF;
