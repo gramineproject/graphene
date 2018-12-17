80e262d24c881e9ac3c993a35b7407d40a828f69
Thomas Knauth <thomas.knauth@intel.com>
2018-12-17 15:48:08 -0800

    Return EOPNOTSUPP instead of EINVAL. Added debug message.

--- LibOS/shim/src/sys/shim_socket.c
+++ LibOS/shim/src/sys/shim_socket.c
@@ -1192,8 +1192,11 @@ int shim_do_sendmmsg (int sockfd, struct mmsghdr * msg, int vlen, int flags)
 static ssize_t do_recvmsg (int fd, struct iovec * bufs, int nbufs, int flags,
                            struct sockaddr * addr, socklen_t * addrlen)
 {
-    /* TODO handle flags properly. For now, just return an error. */
-    if (flags) return -EINVAL;
+    /* TODO handle flags properly. For now, explicitly return an error. */
+    if (flags) {
+        debug("recvmsg()/recvmmsg()/recvfrom(): flags parameter unsupported.\n");
+        return -EOPNOTSUPP;
+    }
     
     struct shim_handle * hdl = get_fd_handle(fd, NULL, NULL);
     if (!hdl)
