ccbc5d9a65ff79e95bbe44abcc6dcc8c2d404736
Gary <gang1.wang@intel.com>
2018-12-26 11:43:32 -0800

    [PAL] Fix insufficient allocation for uri with null termination
    the memory allocation size is insufficient because of omitting the null termination.
    
    Signed-off-by: Gary <gang1.wang@intel.com>

--- Pal/src/host/Linux/db_files.c
+++ Pal/src/host/Linux/db_files.c
@@ -273,7 +273,7 @@ static int file_rename (PAL_HANDLE handle, const char * type,
     if (IS_ERR(ret))
         return unix_to_pal_error(ERRNO(ret));
 
-    handle->file.realpath = malloc_copy(uri, strlen(uri));
+    handle->file.realpath = malloc_copy(uri, strlen(uri) + 1);
     return 0;
 }
 
@@ -491,7 +491,7 @@ static int dir_rename (PAL_HANDLE handle, const char * type,
     if (IS_ERR(ret))
         return unix_to_pal_error(ERRNO(ret));
 
-    handle->dir.realpath = malloc_copy(uri, strlen(uri));
+    handle->dir.realpath = malloc_copy(uri, strlen(uri) + 1);
     return 0;
 }
 
