a0ee5015975044808429ff883db7f0ba15456e8e
Zhang Chen <chen.zhang@intel.com>
2019-02-18 00:24:38 +0800

    shim_namei.c: Fix input absolute path error and add test case
    This bug just occurs when reading a symlink on procfs.
    If input is an absolute path, it will get wrong my_dent.
    So we use stat syscall to check whether the "/proc/1/root"
    and "/" is same path in graphene.
    
    Signed-off-by: Zhang Chen <chen.zhang@intel.com>

---
+++ LibOS/shim/test/regression/40_proc_path.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+
+import os, sys
+from regression import Regression
+
+loader = sys.argv[1]
+
+# Running Bootstrap
+regression = Regression(loader, "proc-path")
+
+regression.add_check(name="Base /proc path present",
+    check=lambda res: "proc path test success" in res[0].out)
+
+rv = regression.run_checks()
+if rv: sys.exit(rv)
