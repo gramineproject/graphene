77fbe2218d7e42eaf54b011323dc96522446e6b8
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2019-02-25 17:40:46 -0800

    [LibOS] execve uses test_user_string to check invalid pointers in path, argv, envp

---
+++ LibOS/shim/test/regression/exec_invalid_args.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main(int argc, const char** argv, const char** envp) {
+    int r;
+    char* badptr = (char*) -1;
+
+    char* bad_argv[]  = { badptr,  NULL };
+    char* good_argv[] = { "DUMMY", NULL };
+
+    char* bad_envp[]  = { badptr,  NULL };
+    char* good_envp[] = { "DUMMY", NULL };
+
+    r = execve(badptr, good_argv, good_envp);
+    if (r == -1 && errno == EFAULT)
+        printf("execv(invalid-path) correctly returned error\n");
+
+    r = execve(argv[0], bad_argv, good_envp);
+    if (r == -1 && errno == EFAULT)
+        printf("execv(invalid-argv) correctly returned error\n");
+
+    r = execve(argv[0], good_argv, bad_envp);
+    if (r == -1 && errno == EFAULT)
+        printf("execv(invalid-envp) correctly returned error\n");
+
+    return 0;
+}
