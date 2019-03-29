f859f3da77dcf6983aa994071c75dac961bb0d6f
Rafał Wojdyła <omeg@invisiblethingslab.com>
2019-03-29 12:58:27 +0100

    [LibOS] Add regression test for getdents

---
+++ LibOS/shim/test/regression/30_getdents.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+
+import os, sys, mmap
+from regression import Regression
+
+loader = sys.argv[1]
+
+# Running Bootstrap
+regression = Regression(loader, "getdents", None, 10000)
+
+# This doesn't catch extraneous entries, but should be fine
+# until the LTP test can be run (need symlink support)
+regression.add_check(name="Directory listing (32-bit)",
+    check=lambda res: "getdents: setup ok" in res[0].out and \
+                      "getdents32: . [0x4]" in res[0].out and \
+                      "getdents32: .. [0x4]" in res[0].out and \
+                      "getdents32: file1 [0x8]" in res[0].out and \
+                      "getdents32: file2 [0x8]" in res[0].out and \
+                      "getdents32: dir3 [0x4]" in res[0].out)
+
+regression.add_check(name="Directory listing (64-bit)",
+    check=lambda res: "getdents: setup ok" in res[0].out and \
+                      "getdents64: . [0x4]" in res[0].out and \
+                      "getdents64: .. [0x4]" in res[0].out and \
+                      "getdents64: file1 [0x8]" in res[0].out and \
+                      "getdents64: file2 [0x8]" in res[0].out and \
+                      "getdents64: dir3 [0x4]" in res[0].out)
+
+rv = regression.run_checks()
+if rv: sys.exit(rv)
