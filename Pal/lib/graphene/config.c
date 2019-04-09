69b138b7741629478b1d29520bd50c5550d47129
Rafał Wojdyła <omeg@invisiblethingslab.com>
2019-04-09 12:16:57 +0200

    Fix free() to accept NULL pointers

--- Pal/lib/graphene/config.c
+++ Pal/lib/graphene/config.c
@@ -373,8 +373,7 @@ int free_config (struct config_store * store)
 {
     struct config * e, * n;
     listp_for_each_entry_safe(e, n, &store->entries, list) {
-        if (e->buf)
-            store->free(e->buf);
+        store->free(e->buf);
         store->free(e);
     }
 
