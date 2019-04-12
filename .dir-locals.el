35cf89a2cd14c14c4c7307302b97df8e35bd9567
Isaku Yamahata <isaku.yamahata@gmail.com>
2019-04-12 15:17:57 -0700

    [.dir-locals.el] add makefile-mode and asm-mode
    For Makefile, tab is needed.
    For .S file, tab is used. (one exception is shim/src/syscall.S. other .S uses tab)
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,5 +3,7 @@
          (fill-column . 100)))
  (c-mode . ((c-file-style . "k&r")
             (c-basic-offset . 4)))
+ (makefile-mode . ((indent-tabs-mode . t)))
+ (asm-mode . ((indent-tabs-mode . t)))
  ((nil . ((truncate-lines . t)))
           (text-mode . ((eval . ((turn-on-auto-fill)))))))
