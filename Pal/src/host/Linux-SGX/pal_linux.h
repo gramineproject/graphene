2c0998174f52002ccab46e22d4752c7d8d31387c
Isaku Yamahata <isaku.yamahata@gmail.com>
2019-04-18 16:54:13 -0700

    [Pal/Linux-SGX] use dedicated stack for signal handler
    In order to handle host signal reliably, allocate stack dedicated for
    signal instead of stack application provides.
    This patch makes host exception handler/ocall exception handler
    properly save/restore fp registers. including initializing them
    right before EEXIT.
    Because size of fp register is large, ocall may cause stack overflow.
    So for ocall case, use dedicated signal too and returning path are
    unified among host exception and ocall exception case.
    Now signal stack is allocated, eframe rewinding is broken and useless.
    So remove it.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@gmail.com>

--- Pal/src/host/Linux-SGX/pal_linux.h
+++ Pal/src/host/Linux-SGX/pal_linux.h
@@ -119,7 +119,7 @@ extern const uint32_t SYNTHETIC_STATE[];
 void init_xsave_size(uint64_t xfrm);
 void save_xregs(PAL_XREGS_STATE* xsave_area);
 void restore_xregs(const PAL_XREGS_STATE* xsave_area);
-noreturn void _restore_sgx_context(sgx_cpu_context_t* uc);
+noreturn void _restore_sgx_context(sgx_cpu_context_t* uc, PAL_XREGS_STATE* xsave_area);
 
 int init_trusted_files (void);
 
