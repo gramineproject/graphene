9a1885085a35ae53536aa2d6cc6e9e020c638b71
Simon Gaiser <simon@invisiblethingslab.com>
2019-04-29 18:58:16 +0200

    [Pal/regression] Test timeout handling when waiting for events

---
+++ Pal/regression/02_Event.py
@@ -0,0 +1,15 @@
+import os, sys
+from regression import Regression
+
+loader = os.environ['PAL_LOADER']
+
+regression = Regression(loader, "Event")
+
+regression.add_check(name="Wait for event with too short timeout",
+    check=lambda res: "Wait with too short timeout ok." in res[0].log)
+
+regression.add_check(name="Wait for event with long enough timeout",
+    check=lambda res: "Wait with long enough timeout ok." in res[0].log)
+
+rv = regression.run_checks()
+if rv: sys.exit(rv)
