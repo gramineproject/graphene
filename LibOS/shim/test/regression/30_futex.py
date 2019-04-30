325f0863ae72ba0bb18f06aad7f8e60daa13c6e3
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2019-04-30 13:55:36 -0700

    fixup! [LibOS/regression] Add error code check

--- LibOS/shim/test/regression/30_futex.py
+++ LibOS/shim/test/regression/30_futex.py
@@ -16,8 +16,7 @@ if rv: sys.exit(rv)
 regression = Regression(loader, "futex-timeout")
 
 regression.add_check(name="Futex Timeout Test",
-    check=lambda res: "hello" in res[0].out and \
-            "world" in res[0].out)
+    check=lambda res: "futex correctly timed out" in res[0].out)
 
 rv = regression.run_checks()
 if rv: sys.exit(rv)
--- LibOS/shim/test/regression/futex-timeout.c
+++ LibOS/shim/test/regression/futex-timeout.c
@@ -3,17 +3,22 @@
 #include <sys/syscall.h>
 #include <linux/futex.h>
 #include <sys/time.h>
+#include <errno.h>
 
 int main (int argc, const char** argv) {
     int myfutex = 0;
+    int ret;
+
     struct timespec t = {
         .tv_sec = 1,
         .tv_nsec = 0
     };
 
-    printf("hello\n");
-    syscall(SYS_futex, &myfutex, FUTEX_WAIT, 0, &t, NULL, 0);
-    printf("world\n");
+    puts("invoke futex syscall with 1-second timeout");
+    ret = syscall(SYS_futex, &myfutex, FUTEX_WAIT, 0, &t, NULL, 0);
+    if (ret == -1 && errno == ETIMEDOUT) {
+        puts("futex correctly timed out");
+    }
 
     return 0;
 }
