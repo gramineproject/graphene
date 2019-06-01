aa4facdf08ed203e7f900102ebf21fc91f772280
Rafał Wojdyła <omeg@invisiblethingslab.com>
2019-06-01 14:53:34 +0200

    Fix whitespaces

--- Pal/lib/api.h
+++ Pal/lib/api.h
@@ -37,14 +37,14 @@ typedef ptrdiff_t ssize_t;
 
 #ifndef MIN
 #define MIN(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-      __typeof__ (b) _b = (b); \
+   ({ __typeof__(a) _a = (a); \
+      __typeof__(b) _b = (b); \
       _a < _b ? _a : _b; })
 #endif
 #ifndef MAX
 #define MAX(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-      __typeof__ (b) _b = (b); \
+   ({ __typeof__(a) _a = (a); \
+      __typeof__(b) _b = (b); \
       _a > _b ? _a : _b; })
 #endif
 
