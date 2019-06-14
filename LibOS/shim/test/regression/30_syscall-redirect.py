5131b09c6b662c5f8317611f73b7f8fbdb2f2c37
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2019-06-14 16:15:29 -0700

    fixup! [LibOS/test] Add condition on SGX-only in Python script

--- LibOS/shim/test/regression/30_syscall-redirect.py
+++ LibOS/shim/test/regression/30_syscall-redirect.py
@@ -2,6 +2,13 @@ import os, sys, mmap
 from regression import Regression
 
 loader = sys.argv[1]
+sgx = os.environ.get('SGX_RUN') == '1'
+
+# This test is only meaningful on SGX PAL because only SGX catches raw syscalls
+# and redirects to Graphene's LibOS. If we will add seccomp to Linux PAL, then
+# we should allow this test on Linux PAL as well.
+if not sgx:
+    sys.exit(0)
 
 # Running Syscall Instruction Example
 regression = Regression(loader, "syscall")
