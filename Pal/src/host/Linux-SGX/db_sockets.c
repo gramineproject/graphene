2b4681ff56693f973c93ff21d8aaad5030cb4c95
alberich1 <alberich1@alberich1.zurich.ibm.com>
2019-06-21 13:16:51 +0200

    Addressing Graphene issue #674
    Socket options initialized to random values in Linux-SGX PAL
    Added:
        memset(&sock_options, 0, sizeof(sock_options));
        sock_options.reuseaddr = 1;
    to each of tcp_connect, tcp_listen, and tcp_accept, udp_bind, and udp_connect
    to initialize sock_options variable.

--- Pal/src/host/Linux-SGX/db_sockets.c
+++ Pal/src/host/Linux-SGX/db_sockets.c
@@ -351,6 +351,11 @@ static int tcp_listen (PAL_HANDLE * handle, char * uri, int options)
 #endif
 
     struct sockopt sock_options;
+
+    // initialize the above sock_options variable
+    memset(&sock_options, 0, sizeof(sock_options));
+    sock_options.reuseaddr = 1;
+
     ret = ocall_sock_listen(bind_addr->sa_family,
                             sock_type(SOCK_STREAM, options), 0,
                             bind_addr, &bind_addrlen,
@@ -386,6 +391,11 @@ static int tcp_accept (PAL_HANDLE handle, PAL_HANDLE * client)
     int ret = 0;
 
     struct sockopt sock_options;
+
+    // initialize the above sock_options variable
+    memset(&sock_options, 0, sizeof(sock_options));
+    sock_options.reuseaddr = 1;
+
     ret = ocall_sock_accept(handle->sock.fd, &dest_addr, &dest_addrlen,
                             &sock_options);
     if (IS_ERR(ret))
@@ -562,6 +572,11 @@ static int udp_bind (PAL_HANDLE * handle, char * uri, int options)
 #endif
 
     struct sockopt sock_options;
+
+    // initialize the above sock_options variable
+    memset(&sock_options, 0, sizeof(sock_options));
+    sock_options.reuseaddr = 1;
+
     ret = ocall_sock_listen(bind_addr->sa_family,
                             sock_type(SOCK_DGRAM, options), 0,
                             bind_addr, &bind_addrlen, &sock_options);
@@ -600,6 +615,11 @@ static int udp_connect (PAL_HANDLE * handle, char * uri, int options)
 #endif
 
     struct sockopt sock_options;
+
+    // initialize the above sock_options variable
+    memset(&sock_options, 0, sizeof(sock_options));
+    sock_options.reuseaddr = 1;
+
     ret = ocall_sock_connect(dest_addr ? dest_addr->sa_family : AF_INET,
                              sock_type(SOCK_DGRAM, options), 0,
                              dest_addr, dest_addrlen,
