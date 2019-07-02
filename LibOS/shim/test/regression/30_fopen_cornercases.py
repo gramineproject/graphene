14102b9f4ef62066503efa81a5faec00166284b0
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2019-07-01 23:51:05 -0700

    fixup! [LibOS] Add special handling in open() syscall (EISDIR, O_TRUNC)

--- LibOS/shim/test/regression/30_fopen_cornercases.py
+++ LibOS/shim/test/regression/30_fopen_cornercases.py
@@ -4,9 +4,9 @@ from regression import Regression
 loader = sys.argv[1]
 
 # Running Long Filepath Example
-regression = Regression(loader, "fs_long_path")
+regression = Regression(loader, "fopen_cornercases")
 
-regression.add_check(name="FS Long Paths",
+regression.add_check(name="fopen corner cases",
         check=lambda res: "Successfully read from file: Hello World" in res[0].out)
 
 rv = regression.run_checks()
