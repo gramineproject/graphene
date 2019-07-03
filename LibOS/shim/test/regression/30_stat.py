a0803a22b4cad839d47a7e1e3bcf0542eafd6f46
Chia-Che Tsai <chiache@tamu.edu>
2019-07-04 07:20:17 +0800

    [LibOS] Add a regression test for fstat() on a directory

--- LibOS/shim/test/regression/30_stat.py
+++ LibOS/shim/test/regression/30_stat.py
@@ -7,10 +7,19 @@ loader = sys.argv[1]
 regression = Regression(loader, "stat_invalid_args")
 
 regression.add_check(name="Stat with invalid arguments",
-    check=lambda res: "stat(invalid-path-ptr) correctly returns error" in res[0].out and \
-                      "stat(invalid-buf-ptr) correctly returns error" in res[0].out and \
-                      "lstat(invalid-path-ptr) correctly returns error" in res[0].out and \
-                      "lstat(invalid-buf-ptr) correctly returns error" in res[0].out)
+    check=lambda res: "stat(invalid-path-ptr) correctly returned error" in res[0].out and \
+                      "stat(invalid-buf-ptr) correctly returned error" in res[0].out and \
+                      "lstat(invalid-path-ptr) correctly returned error" in res[0].out and \
+                      "lstat(invalid-buf-ptr) correctly returned error" in res[0].out)
+
+rv = regression.run_checks()
+if rv: sys.exit(rv)
+
+# Running fstat
+regression = Regression(loader, "fstat_cwd")
+
+regression.add_check(name="Fstat on a directory",
+    check=lambda res: "fstat returned the fd type as S_IFDIR" in res[0].out)
 
 rv = regression.run_checks()
 if rv: sys.exit(rv)
