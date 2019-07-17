ccd625d2c40025283d4cb5e37c2ed9672c851a76
borysp <borysp@invisiblethingslab.com>
2019-07-17 20:45:38 +0200

    [LibOS] Add regression test for reading large directory

---
+++ LibOS/shim/test/regression/large_dir_read.c
@@ -0,0 +1,91 @@
+#define _GNU_SOURCE
+#include <dirent.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+unsigned int FILES_NO = 10000;
+
+int main(int argc, char *argv[]) {
+    int fd = 0, ret = 0;
+    char name[0x10] = { 0 };
+    DIR *dir = NULL;
+    struct dirent *x = NULL;
+    unsigned long i, count = 0;
+    char *tmp_name = NULL;
+
+    setbuf(stdout, NULL);
+    setbuf(stderr, NULL);
+
+    if (argc != 2 && argc != 3) {
+        fprintf(stderr, "Usage: %s tmp_folder_name [files_count]\n", argv[0]);
+        return 1;
+    }
+
+    tmp_name = argv[1];
+
+    if (argc > 2) {
+        FILES_NO = atol(argv[2]);
+    }
+
+    if (mkdir(tmp_name, 0777) < 0 || chdir(tmp_name) < 0) {
+        err(1, "mkdir & chdr");
+    }
+
+    for (i = 0; i < FILES_NO; ++i) {
+        sprintf(name, "%010lu", i);
+        fd = open(name, O_CREAT | O_RDWR, 0777);
+        if (fd < 0) {
+            fprintf(stderr, "cannot create file %lu\n", i);
+            ret = 1;
+            goto cleanup;
+        }
+        close(fd);
+    }
+
+    dir = opendir(".");
+    if (!dir) {
+        fputs("cannot open \".\"", stderr);
+        ret = 1;
+        goto cleanup;
+    }
+
+    while (1) {
+        errno = 0;
+        x = readdir(dir);
+        if (!x) {
+            if (errno != 0) {
+                fprintf(stderr, "error: readdir: %s\n", strerror(errno));
+                ret = 1;
+                goto cleanup;
+            } else {
+                break;
+            }
+        }
+        ++count;
+    }
+
+    printf("count: %lu\n", count);
+
+cleanup:
+    if (dir) {
+        closedir(dir);
+    }
+
+    for (i = 0; i < FILES_NO; ++i) {
+        sprintf(name, "%010lu", i);
+        unlink(name);
+    }
+
+    chdir("..");
+
+    rmdir(tmp_name);
+
+    return ret;
+}
