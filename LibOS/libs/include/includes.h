234fa0a4e07cb02be218b8e6a8acdd7942405b83
Alex Merritt <mail@alexmerritt.us>
2019-07-22 13:20:35 -0700

    [LibOS/libs] Add build area for LibOS libraries
    Creates LibOS/libs/ into which one can build new libraries that are
    preloaded via `loader.preload` manifest option to add functionality to
    LibOS.
    
    Execution starts with the initializers of the library, demonstrated by
    the library in example/
    
    Users can build their library by invoking make inside LibOS/libs/foo if
    their library is named "foo". This will build a dynamic library and
    place it into Runtime/
    
    To make a new library, copy the example directory including the
    Makefile.
    
    Libraries are intentionally NOT automatically built with Graphene; this
    can be changed by adding the library subdirectory name to the makefile
    LibOS/libs/Makefile
    
    Signed-off-by: Alex Merritt <mail@alexmerritt.us>

---
+++ LibOS/libs/include/includes.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <api.h>
+#include <pal/pal.h>
+#include <pal/pal_debug.h>
+
+static inline size_t aligndown(size_t m) {
+    return m & ~(pal_control.alloc_align - 1);
+}
+
+static inline size_t alignup(size_t m) {
+    return aligndown(m + pal_control.alloc_align - 1);
+}
