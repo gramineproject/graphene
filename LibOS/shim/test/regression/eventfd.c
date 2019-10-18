828970f8e3e5aa7e0f1871ce102041e035df2863
Krishnakumar, Sudha <sudha.krishnakumar@intel.com>
2019-10-17 17:01:31 -0700

    Fixed eventfd test code.

--- LibOS/shim/test/regression/eventfd.c
+++ LibOS/shim/test/regression/eventfd.c
@@ -22,7 +22,6 @@ void* write_eventfd_thread(void* arg) {
     uint64_t count = 10;
 
     int* efds = (int*) arg;
-    int i = 0;
 
     if (!arg) {
         printf("arg is NULL\n");
@@ -31,12 +30,12 @@ void* write_eventfd_thread(void* arg) {
 
     printf("%s:got here\n", __func__);
 
-    for (i = 0; i < MAX_EFDS; i++) {
+    for (int i = 0; i < MAX_EFDS; i++) {
         printf("%s: efd = %d\n", __func__, efds[i]);
     }
 
-    for (i = 0; i < MAX_EFDS; i++) {
-        sleep(2);
+    for (int i = 0; i < MAX_EFDS; i++) {
+        sleep(1);
         write(efds[i], &count, sizeof(count));
         count += 1;
     }
@@ -44,50 +43,47 @@ void* write_eventfd_thread(void* arg) {
     return NULL;
 }
 
-/* To support regression testing, positive value returned for error case.
- *
- */
+/* This function used to test polling on a group of eventfd descriptors.
+ * To support regression testing, positive value returned for error case. */
 int eventfd_using_poll() {
     int ret = 0;
     struct pollfd pollfds[MAX_EFDS];
-    pthread_t pid = 0;
+    pthread_t tid = 0;
     uint64_t count = 0;
     int poll_ret = 0;
     int nread_events = 0;
 
-    for (int cnt = 0; cnt < MAX_EFDS; cnt++) {
-        efds[cnt] = eventfd(0, 0);
+    for (int i = 0; i < MAX_EFDS; i++) {
+        efds[i] = eventfd(0, 0);
 
-        if (efds[cnt] < 0) {
+        if (efds[i] < 0) {
             perror("eventfd failed");
             return 1;
         }
 
-        printf("efd = %d\n", efds[cnt]);
+        printf("efd = %d\n", efds[i]);
 
-        pollfds[cnt].fd = efds[cnt];
-        pollfds[cnt].events = POLLIN;
+        pollfds[i].fd = efds[i];
+        pollfds[i].events = POLLIN;
     }
 
-    ret = pthread_create(&pid, NULL, write_eventfd_thread, efds);
+    ret = pthread_create(&tid, NULL, write_eventfd_thread, efds);
 
     if (ret != 0) {
         perror("error in thread creation\n");
         return 1;
-    } else
-        printf("ret=%d, thread spawned fine. \n", ret);
+    }
 
     while (1) {
         poll_ret = poll(pollfds, MAX_EFDS, 5000);
 
         if (poll_ret == 0) {
-            printf("Indicates timeout. Done processing. Exit. \n");
+            printf("Poll timed out. Exiting.\n");
             break;
         }
 
         if (poll_ret < 0) {
             perror("error from poll");
-            printf("error=%d, so exiting\n", errno);
             ret = 1;
             break;
         }
@@ -95,36 +91,34 @@ int eventfd_using_poll() {
         for (int i = 0; i < MAX_EFDS; i++) {
             if (pollfds[i].revents & POLLIN) {
                 pollfds[i].revents = 0;
+                errno = 0;
                 read(pollfds[i].fd, &count, sizeof(count));
                 printf("fd set=%d\n", pollfds[i].fd);
-                printf("parent-pid=%d, efd = %d, count: %lu, errno=%d\n", getpid(), pollfds[i].fd,
+                printf("efd = %d, count: %lu, errno=%d\n", pollfds[i].fd,
                         count, errno);
                 nread_events++;
             }
         }
-
     }
 
     if (nread_events == MAX_EFDS) {
         printf("%s completed successfully\n", __func__);
     } else
-        printf("%s: nread_events=%d, MAX_EFDS=%d\n",
-                __func__, nread_events, MAX_EFDS);
+        printf("%s: nread_events=%d, MAX_EFDS=%d\n", __func__, nread_events, MAX_EFDS);
 
-    pthread_join(pid, NULL);
-
+    pthread_join(tid, NULL);
     return ret;
 }
 
-/* To support regression testing, positive value returned for error case.*/
+/* This function used to test various flags supported while creating eventfd descriptors.
+ * Note: EFD_SEMAPHORE has not been tested.
+ * To support regression testing, positive value returned for error case. */
 int eventfd_using_various_flags() {
-
     uint64_t count = 0;
     int efd = 0;
     int eventfd_flags[] = { 0, EFD_NONBLOCK, EFD_CLOEXEC, EFD_NONBLOCK | EFD_CLOEXEC };
 
     for (int i = 0; i < sizeof(eventfd_flags) / sizeof(int); i++) {
-        count = 5;
         printf("iteration #-%d, flags=%d\n", i, eventfd_flags[i]);
 
         efd = eventfd(0, eventfd_flags[i]);
@@ -134,15 +128,18 @@ int eventfd_using_various_flags() {
             return 1;
         }
 
+        count = 5;
         eventfd_write(efd, count);
         eventfd_write(efd, count);
         count = 0;
+        errno = 0;
         eventfd_read(efd, &count);
         printf("efd = %d, count: %lu, errno=%d\n", efd, count, errno);
-        count = 0;
 
         //Note: Calling another read, will block, if flags dont have EFD_NONBLOCK
         if (eventfd_flags[i] & EFD_NONBLOCK) {
+            count = 0;
+            errno = 0;
             eventfd_read(efd, &count);
             printf("efd = %d, count: %lu, errno=%d\n", efd, count, errno);
         }
@@ -156,7 +153,6 @@ int eventfd_using_various_flags() {
 }
 
 int main(int argc, char* argv[]) {
-
     int ret = 0;
 
     ret = eventfd_using_poll();
