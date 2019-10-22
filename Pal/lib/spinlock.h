90f1f7ddf72e99d3eab2dd7e0ebdd1002f3464a9
borysp <borysp@invisiblethingslab.com>
2019-10-22 16:39:41 +0200

    Add spinlock implementation

---
+++ Pal/lib/spinlock.h
@@ -0,0 +1,40 @@
+#ifndef _SPINLOCK_H
+#define _SPINLOCK_H
+
+typedef int spinlock_t;
+
+/* Use this to initialize spinlocks with *static* storage duration.
+ * According to C standard, there only guarantee we have is that this initialization will happen
+ * before main, which by itself is not enough (such store might not be visible before fist lock
+ * acquire). Fortunately on gcc global zeroed variables will just end up in .bss - zeroed memory
+ * mapped during process creation, hence we are fine. */
+#define INIT_SPINLOCK_UNLOCKED 0
+
+/* Use this to initialize spinlocks with *dynamic* storage duration. */
+static inline void spinlock_init(spinlock_t *lock) {
+    __atomic_store_n(lock, 0, __ATOMIC_RELAXED);
+}
+
+static inline void spinlock_lock(spinlock_t *lock) {
+    int val;
+
+    /* First check if lock is already free. */
+    if (__atomic_exchange_n(lock, 1, __ATOMIC_ACQUIRE) == 0) {
+        return;
+    }
+
+    do {
+        /* This check imposes no inter-thread ordering, thus does not slow other threads. */
+        while (__atomic_load_n(lock, __ATOMIC_RELAXED) != 0)
+            ;
+        /* Seen lock as free, check if it still is, this time with acquire semantics (but only
+         * if we really take it). */
+        val = 0;
+    } while (!__atomic_compare_exchange_n(lock, &val, 1, 1, __ATOMIC_ACQUIRE, __ATOMIC_RELAXED));
+}
+
+static inline void spinlock_unlock(spinlock_t *lock) {
+    __atomic_store_n(lock, 0, __ATOMIC_RELEASE);
+}
+
+#endif // _SPINLOCK_H
