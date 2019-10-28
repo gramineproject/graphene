98fe4511c34935fd9c6601b78c6081f8514428e6
Jia Zhang <zhang.jia@linux.alibaba.com>
2019-10-28 07:08:01 +0000

    [Pal/Linux-SGX] Fix handling sgx.allow_file_creation
    If sgx.allow_file_creation is specified with 0, the resulting behavior
    is unexpected.
    
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- Pal/src/host/Linux-SGX/enclave_framework.c
+++ Pal/src/host/Linux-SGX/enclave_framework.c
@@ -893,10 +893,10 @@ no_trusted:
 no_allowed:
     ret = 0;
 
-    if (get_config(store, "sgx.allow_file_creation", cfgbuf, CONFIG_MAX) <= 0) {
-        allow_file_creation = false;
-    } else
+    if (get_config(store, "sgx.allow_file_creation", cfgbuf, CONFIG_MAX) > 0 && cfgbuf[0] == '1')
         allow_file_creation = true;
+    else
+        allow_file_creation = false;
 
 out:
     free(cfgbuf);
