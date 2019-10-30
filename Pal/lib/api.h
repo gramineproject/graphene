ad428f0d2088ebd888de18f6d6f2c2f27e3b42ee
Krishnakumar, Sudha <sudha.krishnakumar@intel.com>
2019-10-29 18:49:44 -0700

    Fixed it based on comment from Mihal.

--- Pal/lib/api.h
+++ Pal/lib/api.h
@@ -144,9 +144,10 @@ void *calloc(size_t nmemb, size_t size);
 #define force_literal_cstr(str)   ("" str "")
 
 /* check if the var is exactly the same as the static string */
-#define strcmp_static(var, str) (strlen(var) > (static_strlen(force_literal_cstr(str))) ? \
-    (memcmp(var, force_literal_cstr(str), static_strlen(force_literal_cstr(str)) + 1)) : \
-    (memcmp(var, force_literal_cstr(str), strlen(var) + 1)))
+#define strcmp_static(var, str)                                               \
+    (memcmp(var,                                                              \
+            force_literal_cstr(str),                                          \
+            MIN(strlen(var) + 1, static_strlen(force_literal_cstr(str))) + 1))
 
 /* check if the var starts with the static string */
 #define strstartswith_static(var, str) \
