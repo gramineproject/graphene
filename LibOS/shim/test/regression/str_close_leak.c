7af506eced7b61550bf6edc1c0fe811582bc7c3c
Thomas Knauth <thomas.knauth@intel.com>
2019-12-20 10:28:53 -0800

    fixup! Fixup.

--- LibOS/shim/test/regression/str_close_leak.c
+++ LibOS/shim/test/regression/str_close_leak.c
@@ -1,13 +1,13 @@
 #include <fcntl.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int main(int argc, char** argv) {
-    int i;
-    for (i=0; i < 1000000; i++) {
+    for (int i = 0; i < 1000000; i++) {
         int fd = open("/proc/meminfo", O_RDONLY);
-        if (fd == -1) abort();
+        if (fd == -1)
+            abort();
         close(fd);
     }
 
