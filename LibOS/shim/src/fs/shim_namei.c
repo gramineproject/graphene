5154742c1aafea698bd4f5c8690e3d25d53d2588
jack.wxz <jack.wxz@alibaba-inc.com>
2019-12-20 03:21:03 +0000

    fixup! fixup! fixup! bugfix: 1. chroot_open(): update ino after creating a new file 2. chroot_write(): update file size after writing a file of buf_type != FILEBUF_MAP

--- LibOS/shim/src/fs/shim_namei.c
+++ LibOS/shim/src/fs/shim_namei.c
@@ -773,6 +773,10 @@ int list_directory_dentry (struct shim_dentry *dent) {
         child->ino = d->ino;
     }
 
+    /* DEP 12/20/19: Once DENTRY_LISTED is set, the ino of the newly
+       created file will not be updated, so its ino needs to be set
+       in create() or open(O_CREAT).
+     */
     dent->state |= DENTRY_LISTED;
 
 done_read:
