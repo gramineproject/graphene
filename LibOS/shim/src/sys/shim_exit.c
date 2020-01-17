6a6a852fb627faa95eab835e341dc378fbcc1bf6
borysp <borysp@invisiblethingslab.com>
2020-01-17 20:15:49 +0100

    [LibOS] Add error handling to helper threads

--- LibOS/shim/src/sys/shim_exit.c
+++ LibOS/shim/src/sys/shim_exit.c
@@ -140,7 +140,12 @@ noreturn void thread_or_process_exit(int error_code, int term_signal) {
     if (check_last_thread(cur_thread)) {
         /* ask Async Helper thread to cleanup this thread */
         cur_thread->clear_child_tid_pal = 1; /* any non-zero value suffices */
-        install_async_event(NULL, 0, &cleanup_thread, cur_thread);
+        int64_t ret = install_async_event(NULL, 0, &cleanup_thread, cur_thread);
+        if (ret < 0) {
+            debug("failed to set up async cleanup_thread (exiting without clear child tid),"
+                  " return code: %ld\n", ret);
+            DkThreadExit(NULL);
+        }
 
         DkThreadExit(&cur_thread->clear_child_tid_pal);
     }
