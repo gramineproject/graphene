afd8783a387b6a650bc440cbd1f90ef0f1283ed8
Wojtek Porczyk <woju@invisiblethingslab.com>
2020-01-27 15:07:59 +0100

    [Docs] Add logo
    The logo is stolen from https://graphene-project.io/.

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -108,12 +108,10 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
+html_theme_options = {
+    'logo_only': True,
+}
+html_logo = 'graphene_logo.svg'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
