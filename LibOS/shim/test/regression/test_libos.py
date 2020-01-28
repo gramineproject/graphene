2f06208e208e591a1ad9e906ae1b69d940563711
Isaku Yamahata <isaku.yamahata@gmail.com>
2020-01-27 16:21:32 -0800

    [LibOS] sprinkle # pylint: disable=unused-variable for stdout, stderr

--- LibOS/shim/test/regression/test_libos.py
+++ LibOS/shim/test/regression/test_libos.py
@@ -14,13 +14,13 @@ from regression import (
 
 class TC_00_Unittests(RegressionTestCase):
     def test_000_spinlock(self):
-        stdout, stderr = self.run_binary(['spinlock'])
+        stdout, stderr = self.run_binary(['spinlock']) # pylint: disable=unused-variable
 
         self.assertIn('Test successful!', stdout)
 
 class TC_01_Bootstrap(RegressionTestCase):
     def test_100_basic_bootstrapping(self):
-        stdout, stderr = self.run_binary(['bootstrap'])
+        stdout, stderr = self.run_binary(['bootstrap']) # pylint: disable=unused-variable
 
         # Basic Bootstrapping
         self.assertIn('User Program Started', stdout)
@@ -32,7 +32,7 @@ class TC_01_Bootstrap(RegressionTestCase):
 
     def test_101_basic_bootstrapping_five_arguments(self):
         # Five Arguments Given
-        stdout, stderr = self.run_binary(['bootstrap', 'a', 'b', 'c', 'd'])
+        stdout, stderr = self.run_binary(['bootstrap', 'a', 'b', 'c', 'd']) # pylint: disable=unused-variable
         self.assertIn('# of Arguments: 5', stdout)
         self.assertIn('argv[0] = bootstrap', stdout)
         self.assertIn('argv[1] = a', stdout)
@@ -46,24 +46,24 @@ class TC_01_Bootstrap(RegressionTestCase):
         'Linux PAL, then we should allow this test on Linux PAL as well.')
     def test_102_basic_bootstrapping_static(self):
         # bootstrap_static
-        stdout, stderr = self.run_binary(['bootstrap_static'])
+        stdout, stderr = self.run_binary(['bootstrap_static']) # pylint: disable=unused-variable
         self.assertIn('Hello world (bootstrap_static)!', stdout)
 
     def test_103_basic_bootstrapping_pie(self):
         # bootstrap_pie
-        stdout, stderr = self.run_binary(['bootstrap_pie'])
+        stdout, stderr = self.run_binary(['bootstrap_pie']) # pylint: disable=unused-variable
         self.assertIn('User program started', stdout)
         self.assertIn('Local Address in Executable: 0x', stdout)
         self.assertIn('argv[0] = bootstrap_pie', stdout)
 
     def test_110_basic_bootstrapping_cxx(self):
-        stdout, stderr = self.run_binary(['bootstrap-c++'])
+        stdout, stderr = self.run_binary(['bootstrap-c++']) # pylint: disable=unused-variable
 
         # Basic Bootstrapping (C++)
         self.assertIn('User Program Started', stdout)
 
     def test_200_exec(self):
-        stdout, stderr = self.run_binary(['exec'])
+        stdout, stderr = self.run_binary(['exec']) # pylint: disable=unused-variable
 
         # 2 page child binary
         self.assertIn(
@@ -86,25 +86,25 @@ class TC_01_Bootstrap(RegressionTestCase):
             stdout)
 
     def test_201_exec_same(self):
-        stdout, stderr = self.run_binary(['exec_same'])
+        stdout, stderr = self.run_binary(['exec_same']) # pylint: disable=unused-variable
         self.assertIn('hello from execv process', stdout)
 
     def test_202_fork_and_exec(self):
-        stdout, stderr = self.run_binary(['fork_and_exec'])
+        stdout, stderr = self.run_binary(['fork_and_exec']) # pylint: disable=unused-variable
 
         # fork and exec 2 page child binary
         self.assertIn('child exited with status: 0', stdout)
         self.assertIn('test completed successfully', stdout)
 
     def test_203_vfork_and_exec(self):
-        stdout, stderr = self.run_binary(['vfork_and_exec'])
+        stdout, stderr = self.run_binary(['vfork_and_exec']) # pylint: disable=unused-variable
 
         # vfork and exec 2 page child binary
         self.assertIn('child exited with status: 0', stdout)
         self.assertIn('test completed successfully', stdout)
 
     def test_210_exec_invalid_args(self):
-        stdout, stderr = self.run_binary(['exec_invalid_args'])
+        stdout, stderr = self.run_binary(['exec_invalid_args']) # pylint: disable=unused-variable
 
         # Execve with invalid pointers in arguments
         self.assertIn(
@@ -119,7 +119,7 @@ class TC_01_Bootstrap(RegressionTestCase):
             'execve(invalid-envp) correctly returned error', stdout)
 
     def test_300_shared_object(self):
-        stdout, stderr = self.run_binary(['shared_object'])
+        stdout, stderr = self.run_binary(['shared_object']) # pylint: disable=unused-variable
 
         # Shared Object
         self.assertIn('Hello world', stdout)
@@ -154,7 +154,7 @@ class TC_01_Bootstrap(RegressionTestCase):
             self.assertNotEqual(e.returncode, 42, 'expected returncode != 42')
 
     def test_600_multi_pthread(self):
-        stdout, stderr = self.run_binary(['multi_pthread'])
+        stdout, stderr = self.run_binary(['multi_pthread']) # pylint: disable=unused-variable
 
         # Multiple thread creation
         self.assertIn('128 Threads Created', stdout)
@@ -165,7 +165,7 @@ class TC_01_Bootstrap(RegressionTestCase):
     'Linux PAL, then we should allow this test on Linux PAL as well.')
 class TC_02_OpenMP(RegressionTestCase):
     def test_000_simple_for_loop(self):
-        stdout, stderr = self.run_binary(['openmp'])
+        stdout, stderr = self.run_binary(['openmp']) # pylint: disable=unused-variable
 
         # OpenMP simple for loop
         self.assertIn('first: 0, last: 9', stdout)
@@ -176,14 +176,14 @@ class TC_02_OpenMP(RegressionTestCase):
 class TC_03_FileCheckPolicy(RegressionTestCase):
     def test_000_strict_success(self):
         manifest = self.get_manifest('file_check_policy_strict')
-        stdout, stderr = self.run_binary([manifest, 'trusted_testfile'])
+        stdout, stderr = self.run_binary([manifest, 'trusted_testfile']) # pylint: disable=unused-variable
 
         self.assertIn('file_check_policy succeeded', stdout)
 
     def test_001_strict_fail(self):
         manifest = self.get_manifest('file_check_policy_strict')
         try:
-            stdout, stderr = self.run_binary([manifest, 'unknown_testfile'])
+            stdout, stderr = self.run_binary([manifest, 'unknown_testfile']) # pylint: disable=unused-variable
             self.fail('expected to return nonzero')
         except subprocess.CalledProcessError as e:
             self.assertEqual(e.returncode, 2, 'expected returncode == 2')
@@ -204,14 +204,14 @@ class TC_03_FileCheckPolicy(RegressionTestCase):
 
 class TC_30_Syscall(RegressionTestCase):
     def test_000_getcwd(self):
-        stdout, stderr = self.run_binary(['getcwd'])
+        stdout, stderr = self.run_binary(['getcwd']) # pylint: disable=unused-variable
 
         # Getcwd syscall
         self.assertIn('[bss_cwd_buf] getcwd succeeded: /', stdout)
         self.assertIn('[mmapped_cwd_buf] getcwd succeeded: /', stdout)
 
     def test_010_stat_invalid_args(self):
-        stdout, stderr = self.run_binary(['stat_invalid_args'])
+        stdout, stderr = self.run_binary(['stat_invalid_args']) # pylint: disable=unused-variable
 
         # Stat with invalid arguments
         self.assertIn('stat(invalid-path-ptr) correctly returned error', stdout)
@@ -220,7 +220,7 @@ class TC_30_Syscall(RegressionTestCase):
         self.assertIn('lstat(invalid-buf-ptr) correctly returned error', stdout)
 
     def test_011_fstat_cwd(self):
-        stdout, stderr = self.run_binary(['fstat_cwd'])
+        stdout, stderr = self.run_binary(['fstat_cwd']) # pylint: disable=unused-variable
 
         # fstat on a directory
         self.assertIn('fstat returned the fd type as S_IFDIR', stdout)
@@ -229,7 +229,7 @@ class TC_30_Syscall(RegressionTestCase):
         # This doesn't catch extraneous entries, but should be fine
         # until the LTP test can be run (need symlink support)
 
-        stdout, stderr = self.run_binary(['getdents'])
+        stdout, stderr = self.run_binary(['getdents']) # pylint: disable=unused-variable
         self.assertIn('getdents: setup ok', stdout)
 
         # Directory listing (32-bit)
@@ -247,7 +247,7 @@ class TC_30_Syscall(RegressionTestCase):
         self.assertIn('getdents64: dir3 [0x4]', stdout)
 
     def test_021_getdents_large_dir(self):
-        stdout, stderr = self.run_binary(['large_dir_read', 'tmp/large_dir', '3000'])
+        stdout, stderr = self.run_binary(['large_dir_read', 'tmp/large_dir', '3000']) # pylint: disable=unused-variable
 
         self.assertIn('Success!', stdout)
 
@@ -258,7 +258,7 @@ class TC_30_Syscall(RegressionTestCase):
     def test_030_fopen(self):
         if os.path.exists("tmp/filecreatedbygraphene"):
             os.remove("tmp/filecreatedbygraphene")
-        stdout, stderr = self.run_binary(['fopen_cornercases'])
+        stdout, stderr = self.run_binary(['fopen_cornercases']) # pylint: disable=unused-variable
 
         # fopen corner cases
         self.assertIn('Successfully read from file: Hello World', stdout)
@@ -294,7 +294,7 @@ class TC_30_Syscall(RegressionTestCase):
         self.assertIn('Test successful!', stdout)
 
     def test_050_mmap(self):
-        stdout, stderr = self.run_binary(['mmap-file'], timeout=60)
+        stdout, stderr = self.run_binary(['mmap-file'], timeout=60) # pylint: disable=unused-variable
 
         # Private mmap beyond file range
         self.assertIn('mmap test 6 passed', stdout)
@@ -310,14 +310,14 @@ class TC_30_Syscall(RegressionTestCase):
         'On SGX, SIGBUS isn\'t always implemented correctly, for lack '
         'of memory protection. For now, some of these cases won\'t work.')
     def test_051_mmap_sgx(self):
-        stdout, stderr = self.run_binary(['mmap-file'], timeout=60)
+        stdout, stderr = self.run_binary(['mmap-file'], timeout=60) # pylint: disable=unused-variable
 
         # SIGBUS test
         self.assertIn('mmap test 5 passed', stdout)
         self.assertIn('mmap test 8 passed', stdout)
 
     def test_052_large_mmap(self):
-        stdout, stderr = self.run_binary(['large-mmap'], timeout=480)
+        stdout, stderr = self.run_binary(['large-mmap'], timeout=480) # pylint: disable=unused-variable
 
         # Ftruncate
         self.assertIn('large-mmap: ftruncate OK', stdout)
@@ -333,7 +333,7 @@ class TC_30_Syscall(RegressionTestCase):
 
     @unittest.skip('sigaltstack isn\'t correctly implemented')
     def test_060_sigaltstack(self):
-        stdout, stderr = self.run_binary(['sigaltstack'])
+        stdout, stderr = self.run_binary(['sigaltstack']) # pylint: disable=unused-variable
 
         # Sigaltstack Test
         self.assertIn('OK on sigaltstack in main thread before alarm', stdout)
@@ -351,7 +351,7 @@ class TC_30_Syscall(RegressionTestCase):
         self.assertIn('done exiting', stdout)
 
     def test_070_eventfd(self):
-        stdout, stderr = self.run_binary(['eventfd'])
+        stdout, stderr = self.run_binary(['eventfd']) # pylint: disable=unused-variable
 
         # Eventfd Test
         self.assertIn('eventfd_using_poll completed successfully', stdout)
@@ -359,7 +359,7 @@ class TC_30_Syscall(RegressionTestCase):
         self.assertIn('eventfd_using_fork completed successfully', stdout)
 
     def test_080_sched(self):
-        stdout, stderr = self.run_binary(['sched'])
+        stdout, stderr = self.run_binary(['sched']) # pylint: disable=unused-variable
 
         # Scheduling Syscalls Test
         self.assertIn('Test completed successfully', stdout)
@@ -370,14 +370,14 @@ class TC_30_Syscall(RegressionTestCase):
     'Linux PAL, then we should allow this test on Linux PAL as well.')
 class TC_31_SyscallSGX(RegressionTestCase):
     def test_000_syscall_redirect(self):
-        stdout, stderr = self.run_binary(['syscall'])
+        stdout, stderr = self.run_binary(['syscall']) # pylint: disable=unused-variable
 
         # Syscall Instruction Redirection
         self.assertIn('Hello world', stdout)
 
 class TC_40_FileSystem(RegressionTestCase):
     def test_000_base(self):
-        stdout, stderr = self.run_binary(['proc'])
+        stdout, stderr = self.run_binary(['proc']) # pylint: disable=unused-variable
 
         # Base /proc files present
         self.assertIn('/proc/1/..', stdout)
@@ -393,19 +393,19 @@ class TC_40_FileSystem(RegressionTestCase):
         self.assertIn('/proc/cpuinfo', stdout)
 
     def test_010_path(self):
-        stdout, stderr = self.run_binary(['proc-path'])
+        stdout, stderr = self.run_binary(['proc-path']) # pylint: disable=unused-variable
 
         # Base /proc path present
         self.assertIn('proc path test success', stdout)
 
     def test_020_cpuinfo(self):
-        stdout, stderr = self.run_binary(['proc_cpuinfo'], timeout=50)
+        stdout, stderr = self.run_binary(['proc_cpuinfo'], timeout=50) # pylint: disable=unused-variable
 
         # proc/cpuinfo Linux-based formatting
         self.assertIn('cpuinfo test passed', stdout)
 
     def test_030_fdleak(self):
-        stdout, stderr = self.run_binary(['fdleak'], timeout=10)
+        stdout, stderr = self.run_binary(['fdleak'], timeout=10) # pylint: disable=unused-variable
         self.assertIn("Test succeeded.", stdout)
 
     def test_040_str_close_leak(self):
@@ -414,11 +414,11 @@ class TC_40_FileSystem(RegressionTestCase):
 
 class TC_80_Socket(RegressionTestCase):
     def test_000_getsockopt(self):
-        stdout, stderr = self.run_binary(['getsockopt'])
+        stdout, stderr = self.run_binary(['getsockopt']) # pylint: disable=unused-variable
         self.assertIn('getsockopt: Got socket type OK', stdout)
 
     def test_010_epoll_wait_timeout(self):
-        stdout, stderr = self.run_binary(['epoll_wait_timeout', '8000'],
+        stdout, stderr = self.run_binary(['epoll_wait_timeout', '8000'], # pylint: disable=unused-variable
             timeout=50)
 
         # epoll_wait timeout
@@ -445,7 +445,7 @@ class TC_80_Socket(RegressionTestCase):
         self.assertIn('pselect() on read event returned 1 file descriptors', stdout)
 
     def test_100_socket_unix(self):
-        stdout, stderr = self.run_binary(['unix'])
+        stdout, stderr = self.run_binary(['unix']) # pylint: disable=unused-variable
         self.assertIn('Data: This is packet 0', stdout)
         self.assertIn('Data: This is packet 1', stdout)
         self.assertIn('Data: This is packet 2', stdout)
@@ -458,7 +458,7 @@ class TC_80_Socket(RegressionTestCase):
         self.assertIn('Data: This is packet 9', stdout)
 
     def test_200_socket_udp(self):
-        stdout, stderr = self.run_binary(['udp'], timeout=50)
+        stdout, stderr = self.run_binary(['udp'], timeout=50) # pylint: disable=unused-variable
         self.assertIn('Data: This is packet 0', stdout)
         self.assertIn('Data: This is packet 1', stdout)
         self.assertIn('Data: This is packet 2', stdout)
