d71d023dccf5a5333725c3253c81c934e8461e1b
Isaku Yamahata <isaku.yamahata@gmail.com>
2020-02-11 15:16:59 -0800

    [Pal/Linux-SGX] fix file_map() to not call ocall_mmap_untrusted() unnecessary
    Some how file_map() is unnecessarily convoluted. sort it out.

--- Pal/src/host/Linux-SGX/db_files.c
+++ Pal/src/host/Linux-SGX/db_files.c
@@ -180,7 +180,6 @@ static int file_map(PAL_HANDLE handle, void** addr, int prot, uint64_t offset, u
     sgx_stub_t* stubs = (sgx_stub_t*)handle->file.stubs;
     uint64_t total    = handle->file.total;
     void* mem         = *addr;
-    void* umem;
     int ret;
 
     /*
@@ -208,37 +207,35 @@ static int file_map(PAL_HANDLE handle, void** addr, int prot, uint64_t offset, u
     if (!mem)
         return -PAL_ERROR_NOMEM;
 
-    uint64_t end = (offset + size > total) ? total : offset + size;
-    uint64_t map_start, map_end;
-
     if (stubs) {
-        map_start = ALIGN_DOWN(offset, TRUSTED_STUB_SIZE);
-        map_end   = ALIGN_UP(end, TRUSTED_STUB_SIZE);
-    } else {
-        map_start = ALLOC_ALIGN_DOWN(offset);
-        map_end   = ALLOC_ALIGN_UP(end);
-    }
+        uint64_t end = (offset + size > total) ? total : offset + size;
+        uint64_t map_start = ALIGN_DOWN(offset, TRUSTED_STUB_SIZE);
+        uint64_t map_end = ALIGN_UP(end, TRUSTED_STUB_SIZE);
 
-    ret = ocall_mmap_untrusted(handle->file.fd, map_start, map_end - map_start, PROT_READ, &umem);
-    if (IS_ERR(ret)) {
-        SGX_DBG(DBG_E, "file_map - ocall returned %d\n", ret);
-        return unix_to_pal_error(ERRNO(ret));
-    }
-
-    if (stubs) {
-        ret = copy_and_verify_trusted_file(handle->file.realpath, umem, map_start, map_end, mem,
-                                           offset, end - offset, stubs, total);
+        ret = copy_and_verify_trusted_file(handle->file.realpath, handle->file.umem + map_start, map_start, map_end,
+                                           mem, offset, end - offset, stubs, total);
 
         if (ret < 0) {
             SGX_DBG(DBG_E, "file_map - verify trusted returned %d\n", ret);
-            ocall_munmap_untrusted(umem, map_end - map_start);
             return ret;
         }
     } else {
-        memcpy(mem, umem + (offset - map_start), end - offset);
+        uint64_t end = offset + size;
+        uint64_t map_start = ALLOC_ALIGN_DOWN(offset);
+        uint64_t map_end   = ALLOC_ALIGN_UP(end);
+        uint64_t map_size = map_end - map_start;
+
+        ssize_t ret = __ocall_pread(handle->file.fd, mem, map_size, map_start, /*interruptible=*/false);
+        if (IS_ERR(ret)) {
+            assert(ERRNO(ret) != EINTR);
+            SGX_DBG(DBG_E, "file_map - ocall_pread returned %ld\n", ret);
+            return unix_to_pal_error(ERRNO(ret));
+        }
+        if (map_size - ret > 0) {
+            memset(mem + ret, 0, map_size - ret);
+        }
     }
 
-    ocall_munmap_untrusted(umem, map_end - map_start);
     *addr = mem;
     return 0;
 }
