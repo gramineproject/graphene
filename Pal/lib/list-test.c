086a1cf0dfe8f21ed82ee27b2988c9e5681161fc
Krishnakumar, Sudha <sudha.krishnakumar@intel.com>
2020-03-12 19:48:56 -0700

    Added test code for sorting list nodes in list-test.c

--- Pal/lib/list-test.c
+++ Pal/lib/list-test.c
@@ -30,6 +30,12 @@ struct simple {
 };
 
 DEFINE_LISTP(simple);
+
+static bool compare_node(const void* first, const void* second);
+static void add_node(int idx, LISTP_TYPE(simple)* the_list);
+static void destroy_list(LISTP_TYPE(simple)*  the_list);
+static int test_list_sort();
+
 static LISTP_TYPE(simple) list_in_the_sky      = LISTP_INIT;
 static LISTP_TYPE(simple) list_in_the_basement = LISTP_INIT;
 
@@ -43,11 +49,13 @@ int sol6[4]  = {7, 5, 3, 1};
 int sol7[10] = {7, 5, 3, 1, 13, 12, 11, 10, 9, 8};
 int sol8[17] = {7, 5, 3, 1, 13, 12, 11, 10, 9, 8, 20, 19, 18, 17, 16, 15, 14};
 
-void print_list(LISTP_TYPE(simple)* listp) {
-    struct simple* tmp;
+void print_list(LISTP_TYPE(simple)* the_list) {
+  struct simple *f, *n;
+  if (!the_list)
+    return;
     printf("Beginning of list\n");
-    LISTP_FOR_EACH_ENTRY(tmp, listp, list) {
-        printf("List element %d\n", tmp->idx);
+    LISTP_FOR_EACH_ENTRY_SAFE(f, n, the_list, list) {
+        printf("List element %d\n", f->idx);
     }
     printf("End of list\n\n");
 }
@@ -86,6 +94,7 @@ int main() {
     struct simple* tmp;
     struct simple* tmp2;
     struct simple* n;
+    int sort_ret = 0;
 
     assert(LISTP_EMPTY(&list_in_the_sky));
 
@@ -314,6 +323,138 @@ int main() {
     printf("\nfin\n");
     */
 
+    sort_ret = test_list_sort();
+    assert(sort_ret == 0);
+    printf("Verified list sorting.\n\n");
+
     printf("All tests passed!\n");
     return 0;
+}
+
+/* compare_node function, used to sort the list. */
+static bool compare_node(const void* first, const void* second) {
+    if (!first || !second)
+        return false;
+
+    return (((struct simple* )first)->idx <
+                ((struct simple* )second)->idx);
+}
+
+static void add_node(int idx, LISTP_TYPE(simple)* the_list) {
+    struct simple* list_node = NULL;
+    size_t len = 0;
+    if (!the_list)
+        return;
+    list_node = (struct simple*)malloc(sizeof(struct simple));
+    if (!list_node)
+        return;
+    list_node->idx = idx;
+    LISTP_ADD_TAIL(list_node, the_list, list);
+    /*printf("%s: added list_node_ptr=%p, idx->%d\n", __func__, list_node, list_node->idx);*/
+}
+
+static void destroy_list(LISTP_TYPE(simple)*  the_list) {
+    LISTP_CLEAR_AND_FREE_EACH_LIST_ITEM(the_list, simple);
+}
+
+static int verify_sort(LISTP_TYPE(simple)*  the_list, int *sorted_arr, int len) {
+  struct simple *f, *n;
+  int i = 0;
+
+  if (!the_list)
+    return -1;
+
+  LISTP_FOR_EACH_ENTRY_SAFE(f, n, the_list, list) {
+    if (f->idx != sorted_arr[i]) {
+        printf("list val=%d, does not match sorted arr val=%d\n", f->idx, sorted_arr[i]);
+        return -1;
+    }
+    else {
+        i = (i < len - 1) ? (i + 1):i;
+    }
+  }
+  if (len != (i + 1)) {
+    printf("list len=%d, expected len=%d\n", i + 1, len);
+    return -1;
+  }
+    return 0;
+}
+
+static int test_list_sort() {
+    #define NUM_OF_INPUTS 11
+
+    typedef struct _arr_in {
+        int *arr;
+        size_t arr_len;
+    }arr_in_t;
+
+    int sort_ret = 0;
+
+    int arr1[] = {20};
+    int arr2[] = {20, 10};
+    int arr3[] = {20, 10, 30};
+    int arr4[] = {20, 10, 30, 5};
+    int arr5[] = {5, 20, 10, 30};
+    int arr6[] = {10, 20, 30, 40, 15, 50, 60, 70};
+    int arr7[] = {10, 40, 30, 20, 5, 70, 60, 50};
+    int arr8[] = {10, 20, 40, 50, 70, 90, 101, 201, 301};
+    int arr9[] = {100, 20, 40, 30, 50, 70, 60, 90, 80, 10};
+    int arr10[] = {100, 20, 40, 30, 80, 50, 70, 60, 90, 80, 10, 101, 201, 301};
+    int arr11[] = {11, 50, 30, 11, 20, 2, 12, 2, 5, 70, 7, 8, 7, 8, 12, 50, 1};
+
+    int sorted_arr1[] = {20};
+    int sorted_arr2[] = {10, 20};
+    int sorted_arr3[] = {10, 20, 30};
+    int sorted_arr4[] = {5, 10, 20, 30};
+    int sorted_arr5[] = {5, 10, 20, 30};
+    int sorted_arr6[] = {10, 15, 20, 30, 40, 50, 60, 70};
+    int sorted_arr7[] = {5, 10, 20, 30, 40, 50, 60, 70};
+    int sorted_arr8[] = {10, 20, 40, 50, 70, 90, 101, 201, 301};
+    int sorted_arr9[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int sorted_arr10[] = {10, 20, 30, 40, 50, 60, 70, 80, 80, 90, 100, 101, 201, 301};
+    int sorted_arr11[] = {1, 2, 2, 5, 7, 7, 8, 8, 11, 11, 12, 12, 20, 30, 50, 50, 70};
+
+    arr_in_t arr_in[NUM_OF_INPUTS] = {{arr1, sizeof(arr1)/sizeof(int)},
+            {arr2, sizeof(arr2)/sizeof(int)}, {arr3, sizeof(arr3)/sizeof(int)},
+            {arr4, sizeof(arr4)/sizeof(int)}, {arr5, sizeof(arr5)/sizeof(int)},
+            {arr6, sizeof(arr6)/sizeof(int)}, {arr7, sizeof(arr7)/sizeof(int)},
+            {arr8, sizeof(arr8)/sizeof(int)}, {arr9, sizeof(arr9)/sizeof(int)},
+            {arr10, sizeof(arr10)/sizeof(int)}, {arr11, sizeof(arr11)/sizeof(int)}};
+
+    arr_in_t sorted_arr_in[NUM_OF_INPUTS] = {{sorted_arr1, sizeof(sorted_arr1)/sizeof(int)},
+    {sorted_arr2, sizeof(sorted_arr2)/sizeof(int)}, {sorted_arr3, sizeof(sorted_arr3)/sizeof(int)},
+    {sorted_arr4, sizeof(sorted_arr4)/sizeof(int)}, {sorted_arr5, sizeof(sorted_arr5)/sizeof(int)},
+    {sorted_arr6, sizeof(sorted_arr6)/sizeof(int)}, {sorted_arr7, sizeof(sorted_arr7)/sizeof(int)},
+    {sorted_arr8, sizeof(sorted_arr8)/sizeof(int)}, {sorted_arr9, sizeof(sorted_arr9)/sizeof(int)},
+    {sorted_arr10, sizeof(sorted_arr10)/sizeof(int)}, {sorted_arr11, sizeof(sorted_arr11)/sizeof(int)}};
+
+    for (int cnt = 0; cnt < NUM_OF_INPUTS; cnt++) {
+        size_t size = arr_in[cnt].arr_len;
+        size_t list_len = 0;
+        if (size != sorted_arr_in[cnt].arr_len) {
+            printf("size mismatch, arr_in size=%lu, sorted arr size=%lu\n",
+            size, sorted_arr_in[cnt].arr_len);
+            return -1;
+        }
+
+        LISTP_TYPE(simple) the_list;
+        INIT_LISTP(&the_list);
+        LISTP_SET_COMPARE_FUNCTION(&the_list, compare_node);
+
+        for (int i = 0; i < size; i++) {
+            add_node(arr_in[cnt].arr[i], &the_list);
+        }
+        LISTP_GET_SIZE(&the_list, simple, list_len);
+        if (list_len != size) {
+            printf("length mismatch. ERROR, list_len=%lu, size=%lu\n",
+            list_len, size);
+            destroy_list(&the_list);
+            return -1;
+        }
+        LISTP_SORT(&the_list, simple);
+        /*print_list(&the_list);*/
+        sort_ret += verify_sort(&the_list, sorted_arr_in[cnt].arr, size);
+        destroy_list(&the_list);
+    }
+    return sort_ret;
 }
