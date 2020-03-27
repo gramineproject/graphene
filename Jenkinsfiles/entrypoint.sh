b261ff083bae71cdb21ac64276e699b9d909fa60
Anjo Vahldiek-Oberwagner <anjovahldiek@gmail.com>
2020-03-26 20:39:42 -0700

    fixup! adding entrypoint script

--- Jenkinsfiles/entrypoint.sh
+++ Jenkinsfiles/entrypoint.sh
@@ -5,12 +5,12 @@ set -e
 userid=`id -u $USER`
 rootid=`id -u root`
 if test "$userid" != "$rootid" ; then
-    exec $@
+    exec "$@"
 else
     if test -e "/var/run/docker.sock" ; then
         groupmod -g $(stat -c '%g' /var/run/docker.sock) docker
     fi
     usermod -aG docker leeroy
 
-    exec gosu leeroy $@
+    exec gosu leeroy "$@"
 fi
