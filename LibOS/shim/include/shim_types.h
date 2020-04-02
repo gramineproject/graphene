c04e6ed0e55121bfb696257e27afe2aab7b0748a
Stefan Berger <stefanb@linux.ibm.com>
2020-04-02 14:42:06 +0000

    LibOS: Add sockaddr_storage
    Add sockaddr_storage from glibc's bits/socket.h and bits/sockaddr.h.

--- LibOS/shim/include/shim_types.h
+++ LibOS/shim/include/shim_types.h
@@ -379,6 +379,24 @@ struct sockaddr {
     char sa_data[14];           /* Address data.  */
 };
 
+/* From bits/sockaddr.h */
+#define __SOCKADDR_COMMON_SIZE (sizeof(unsigned short int))
+
+/* Size of struct sockaddr_storage.  */
+#define _SS_SIZE 128
+
+/* From bits/socket.h */
+/* Structure large enough to hold any socket address (with the historical
+   exception of AF_UNIX).  */
+#define __ss_aligntype  unsigned long int
+#define _SS_PADSIZE (_SS_SIZE - __SOCKADDR_COMMON_SIZE - sizeof(__ss_aligntype))
+
+struct sockaddr_storage {
+    __SOCKADDR_COMMON(ss_);    /* Address family, etc.  */
+    char __ss_padding[_SS_PADSIZE];
+    __ss_aligntype __ss_align;  /* Force desired alignment.  */
+};
+
 /* linux/mqueue.h */
 struct __kernel_mq_attr {
     long    mq_flags;       /* message queue flags */
