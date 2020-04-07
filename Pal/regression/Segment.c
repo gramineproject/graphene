9e0f36d2679f7a2e8ca5ceaa778b4eacdf39e1d2
Stefan Berger <stefanb@us.ibm.com>
2020-04-07 12:50:56 +0000

    Pal: Do some cleanup in the moved test cases
    Cleanup in some of the test cases and remove some that are redundant.

--- Pal/regression/Segment.c
+++ Pal/regression/Segment.c
@@ -1,12 +1,10 @@
-/* This Hello World simply print out "Hello World" */
-
 #include "pal.h"
 #include "pal_debug.h"
 
-void* private = &private;
+void* dummy = &dummy;
 
 int main(int argc, char** argv, char** envp) {
-    DkSegmentRegister(PAL_SEGMENT_FS, private);
+    DkSegmentRegister(PAL_SEGMENT_FS, dummy);
     void* ptr;
     __asm__ volatile("mov %%fs:0, %0" : "=r"(ptr)::"memory");
     pal_printf("TLS = %p\n", ptr);
