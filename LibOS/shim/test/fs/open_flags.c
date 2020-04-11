b530884685b017f7d559a234500cd8a063b67451
Rafał Wojdyła <omeg@invisiblethingslab.com>
2020-04-11 16:26:25 +0200

    fixup! fixup! [LibOS] Add open_flags FS test

--- LibOS/shim/test/fs/open_flags.c
+++ LibOS/shim/test/fs/open_flags.c
@@ -1,28 +1,7 @@
 #include "common.h"
 
-int g_mode = 0664;
-char g_data = 'x';
-
-#define OPEN_TEST(flags, exists, expect_success, do_write) {    \
-    char* s = exists ? "[exists]" : "[doesn't exist]";          \
-    fd = open(argv[1], flags, g_mode);                          \
-    if (fd < 0) {                                               \
-        if (expect_success) {                                   \
-            fatal_error("open(" #flags ") %s failed!\n", s);    \
-        } else {                                                \
-            printf("open(" #flags ") %s failed OK\n", s);       \
-        }                                                       \
-    } else {                                                    \
-        if (expect_success) {                                   \
-            printf("open(" #flags ") %s succeeded OK\n", s);    \
-            if (do_write)                                       \
-                write_fd(argv[1], fd, &g_data, sizeof(g_data)); \
-        } else {                                                \
-            fatal_error("open(" #flags ") %s succeeded!\n", s); \
-        }                                                       \
-        close(fd);                                              \
-    }                                                           \
-}
+const int g_mode = 0664;
+const char g_data = 'x';
 
 size_t get_file_size(const char* path) {
     struct stat st;
@@ -32,21 +11,48 @@ size_t get_file_size(const char* path) {
     return st.st_size;
 }
 
+void open_test__(const char* path, int flags, const char* flags_str, bool exists,
+                 bool expect_success, bool do_write) {
+    const char* exists_str = exists ? "exists" : "doesn't exist";
+    int fd = open(path, flags, g_mode);
+    if (fd < 0) {
+        if (expect_success) {
+            fatal_error("open(%s) [%s] failed!\n", flags_str, exists_str);
+        } else {
+            printf("open(%s) [%s] failed as expected\n", flags_str, exists_str);
+        }
+    } else {
+        if (expect_success) {
+            printf("open(%s) [%s] succeeded as expected\n", flags_str, exists_str);
+            if (do_write)
+                write_fd(path, fd, &g_data, sizeof(g_data));
+        } else {
+            fatal_error("open(%s) [%s] unexpectedly succeeded\n", flags_str, exists_str);
+        }
+        close(fd);
+    }
+}
+
+#define OPEN_TEST(path, flags, exists, expect_success, do_write) \
+    open_test__(path, flags, #flags, exists, expect_success, do_write)
+
 int main(int argc, char* argv[]) {
     if (argc < 2)
         fatal_error("Usage: %s <path>\n", argv[0]);
 
     setup();
-    int fd = -1;
 
     // doesn't exist - should create
-    OPEN_TEST(O_CREAT|O_EXCL|O_RDWR, /*exists=*/false, /*expect_success=*/true, /*do_write=*/true);
+    OPEN_TEST(argv[1], O_CREAT|O_EXCL|O_RDWR, /*exists=*/false, /*expect_success=*/true,
+              /*do_write=*/true);
 
     // exists - open should fail
-    OPEN_TEST(O_CREAT|O_EXCL|O_RDWR, /*exists=*/true, /*expect_success=*/false, /*do_write=*/false);
+    OPEN_TEST(argv[1], O_CREAT|O_EXCL|O_RDWR, /*exists=*/true, /*expect_success=*/false,
+              /*do_write=*/false);
 
     // exists - should open existing and NOT truncate
-    OPEN_TEST(O_CREAT|O_RDWR, /*exists=*/true, /*expect_success=*/true, /*do_write=*/false);
+    OPEN_TEST(argv[1], O_CREAT|O_RDWR, /*exists=*/true, /*expect_success=*/true,
+              /*do_write=*/false);
     if (get_file_size(argv[1]) != 1)
         fatal_error("File was truncated\n");
 
@@ -54,16 +60,19 @@ int main(int argc, char* argv[]) {
         fatal_error("unlink(%s) failed: %s\n", argv[1], strerror(errno));
 
     // doesn't exist - should create new
-    OPEN_TEST(O_CREAT|O_RDWR, /*exists=*/false, /*expect_success=*/true, /*do_write=*/false);
+    OPEN_TEST(argv[1], O_CREAT|O_RDWR, /*exists=*/false, /*expect_success=*/true,
+              /*do_write=*/false);
 
     if (unlink(argv[1]) < 0)
         fatal_error("unlink(%s) failed: %s\n", argv[1], strerror(errno));
 
     // doesn't exist - should create new
-    OPEN_TEST(O_CREAT|O_TRUNC|O_RDWR, /*exists=*/false, /*expect_success=*/true, /*do_write=*/true);
+    OPEN_TEST(argv[1], O_CREAT|O_TRUNC|O_RDWR, /*exists=*/false, /*expect_success=*/true,
+              /*do_write=*/true);
 
     // exists - should truncate
-    OPEN_TEST(O_CREAT|O_TRUNC|O_RDWR, /*exists=*/true, /*expect_success=*/true, /*do_write=*/false);
+    OPEN_TEST(argv[1], O_CREAT|O_TRUNC|O_RDWR, /*exists=*/true, /*expect_success=*/true,
+              /*do_write=*/false);
     if (get_file_size(argv[1]) != 0)
         fatal_error("File was not truncated\n");
 
