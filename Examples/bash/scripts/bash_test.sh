db8d7a6096ae199d7341b432ef485582426336a8
borysp <borysp@invisiblethingslab.com>
2020-04-22 00:21:24 +0200

    TMP commit; debugging Jenkins issues

--- Examples/bash/scripts/bash_test.sh
+++ Examples/bash/scripts/bash_test.sh
@@ -7,8 +7,13 @@ for (( c=1; c<=$times; c++ ))
 do
 	echo "hello $c"
 	cp somefile testdir/somefile
+	echo "cp"
 	cat somefile > testdir/createdfile
+	echo "cat"
 	ls testdir/
+	echo "ls"
 	rm -rf testdir/somefile testdir/createdfile
+	echo "rm"
 	date +"current date is %D"
+	echo "date"
 done
