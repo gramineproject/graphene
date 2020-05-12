1ac229fc81995002fe0f8aef157fac3339ef7f33
Stefan Berger <stefanb@us.ibm.com>
2020-05-12 00:17:53 +0000

    fixup! [Pal] Fix IPv6 related failures by using sockaddr_storage

--- Pal/src/host/Linux/db_sockets.c
+++ Pal/src/host/Linux/db_sockets.c
@@ -329,7 +329,7 @@ static bool check_any_addr(struct sockaddr* addr) {
 /* listen on a tcp socket */
 static int tcp_listen(PAL_HANDLE* handle, char* uri, int create, int options) {
     struct sockaddr_storage buffer;
-    struct sockaddr* bind_addr = (struct sockaddr *)&buffer;
+    struct sockaddr* bind_addr = (struct sockaddr*)&buffer;
     size_t bind_addrlen = sizeof(buffer);
     int ret, fd = -1;
 
@@ -434,7 +434,7 @@ static int tcp_accept(PAL_HANDLE handle, PAL_HANDLE* client) {
                 return unix_to_pal_error(ERRNO(newfd));
         }
 
-    struct sockaddr* dest_addr = (struct sockaddr *)&buffer;
+    struct sockaddr* dest_addr = (struct sockaddr*)&buffer;
     size_t dest_addrlen        = addrlen;
 
     *client = socket_create_handle(pal_type_tcp, newfd, 0, bind_addr, bind_addrlen, dest_addr,
@@ -455,9 +455,9 @@ failed:
 /* connect on a tcp socket */
 static int tcp_connect(PAL_HANDLE* handle, char* uri, int options) {
     struct sockaddr_storage buffer[3];
-    struct sockaddr* bind_addr = (struct sockaddr *)&buffer[0];
+    struct sockaddr* bind_addr = (struct sockaddr*)&buffer[0];
     size_t bind_addrlen = sizeof(buffer[0]);
-    struct sockaddr* dest_addr = (struct sockaddr *)&buffer[1];
+    struct sockaddr* dest_addr = (struct sockaddr*)&buffer[1];
     size_t dest_addrlen = sizeof(buffer[1]);
     int ret, fd = -1;
 
@@ -509,7 +509,7 @@ static int tcp_connect(PAL_HANDLE* handle, char* uri, int options) {
 
     if (!bind_addr) {
         /* save some space to get socket address */
-        bind_addr    = (struct sockaddr *)&buffer[2];
+        bind_addr    = (struct sockaddr*)&buffer[2];
         bind_addrlen = sizeof(buffer[2]);
 
         /* call getsockname to get socket address */
@@ -627,7 +627,7 @@ static int64_t tcp_write(PAL_HANDLE handle, uint64_t offset, size_t len, const v
 /* used by 'open' operation of tcp stream for bound socket */
 static int udp_bind(PAL_HANDLE* handle, char* uri, int create, int options) {
     struct sockaddr_storage buffer;
-    struct sockaddr* bind_addr = (struct sockaddr *)&buffer;
+    struct sockaddr* bind_addr = (struct sockaddr*)&buffer;
     size_t bind_addrlen = sizeof(buffer);
     int ret = 0, fd = -1;
 
@@ -694,9 +694,9 @@ failed:
 /* used by 'open' operation of tcp stream for connected socket */
 static int udp_connect(PAL_HANDLE* handle, char* uri, int create, int options) {
     struct sockaddr_storage buffer[2];
-    struct sockaddr* bind_addr = (struct sockaddr *)&buffer[0];
+    struct sockaddr* bind_addr = (struct sockaddr*)&buffer[0];
     size_t bind_addrlen = sizeof(buffer[0]);
-    struct sockaddr* dest_addr = (struct sockaddr *)&buffer[1];
+    struct sockaddr* dest_addr = (struct sockaddr*)&buffer[1];
     size_t dest_addrlen = sizeof(buffer[1]);
     int ret, fd = -1;
 
@@ -852,7 +852,7 @@ static int64_t udp_receivebyaddr(PAL_HANDLE handle, uint64_t offset, size_t len,
         return -PAL_ERROR_OVERFLOW;
 
     int ret = inet_create_uri(addr_uri, addr + addrlen - addr_uri,
-                              (struct sockaddr *)&conn_addr, hdr.msg_namelen);
+                              (struct sockaddr*)&conn_addr, hdr.msg_namelen);
     if (ret < 0)
         return ret;
 
@@ -911,7 +911,7 @@ static int64_t udp_sendbyaddr(PAL_HANDLE handle, uint64_t offset, size_t len, co
     struct sockaddr_storage conn_addr;
     size_t conn_addrlen = sizeof(conn_addr);
 
-    int ret = inet_parse_uri(&addrbuf, (struct sockaddr *)&conn_addr, &conn_addrlen);
+    int ret = inet_parse_uri(&addrbuf, (struct sockaddr*)&conn_addr, &conn_addrlen);
     if (ret < 0)
         return ret;
 
