76c6aa7bc3e8cf2b8728e9baaf2ef4f7e4dcb40c
Stefan Berger <stefanb@us.ibm.com>
2020-06-05 12:01:40 +0000

    fixup! [PAL] lib: Implement strstr

--- Pal/lib/string/strstr.c
+++ Pal/lib/string/strstr.c
@@ -19,19 +19,21 @@
 const char* strstr(const char* haystack, const char* needle) {
     size_t h_len = strlen(haystack);
     size_t n_len = strlen(needle);
-    char* r = NULL;
     unsigned int o = 0;
 
     if (n_len == 0)
         return haystack;
 
+    if (h_len < n_len)
+        return NULL;
+
     while (o <= h_len - n_len) {
-        unsigned int i = 0;
+        size_t i = 0;
         while (i < n_len && haystack[o + i] == needle[i])
             i++;
         if (i == n_len)
             return &haystack[o];
         o++;
     }
-    return r;
+    return NULL;
 }
