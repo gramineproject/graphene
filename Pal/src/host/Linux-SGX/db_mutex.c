8f19e7ee399693e07d1293175d01705a5419e838
Stefan Berger <stefanb@us.ibm.com>
2020-06-13 00:03:57 +0000

    fixup! [PAL/PAL-SGX] Convert futexes to ints

--- Pal/src/host/Linux-SGX/db_mutex.c
+++ Pal/src/host/Linux-SGX/db_mutex.c
@@ -61,8 +61,8 @@ int _DkMutexLockTimeout(struct mutex_handle* m, int64_t timeout_us) {
     int ret = 0;
 
     int t = MUTEX_UNLOCKED;
-    if (__atomic_compare_exchange_n(&m->locked, &t, MUTEX_LOCKED, false,
-                                    __ATOMIC_SEQ_CST, __ATOMIC_RELAXED))
+    if (__atomic_compare_exchange_n(&m->locked, &t, MUTEX_LOCKED, /*weak=*/true,
+                                    __ATOMIC_ACQUIRE, __ATOMIC_RELAXED))
         goto success;
 
     if (timeout_us == 0) {
@@ -74,9 +74,9 @@ int _DkMutexLockTimeout(struct mutex_handle* m, int64_t timeout_us) {
     atomic_inc(&m->nwaiters);
 
     while (true) {
-        int t = MUTEX_UNLOCKED ;
-        if (__atomic_compare_exchange_n(&m->locked, &t, MUTEX_LOCKED, false,
-            __ATOMIC_SEQ_CST, __ATOMIC_RELAXED))
+        int t = MUTEX_UNLOCKED;
+        if (__atomic_compare_exchange_n(&m->locked, &t, MUTEX_LOCKED, /*weak=*/true,
+                                        __ATOMIC_ACQUIRE, __ATOMIC_RELAXED))
             break;
         /*
          * Chia-Che 12/7/2017: m->locked points to untrusted memory, so
@@ -118,7 +118,7 @@ int _DkMutexUnlock(struct mutex_handle* m) {
     int need_wake;
 
     /* Unlock */
-    __atomic_store_n(m->locked, MUTEX_UNLOCKED, __ATOMIC_SEQ_CST);
+    __atomic_store_n(m->locked, MUTEX_UNLOCKED, __ATOMIC_RELEASE);
     /* We need to make sure the write to locked is visible to lock-ers
      * before we read the waiter count. */
     MB();
