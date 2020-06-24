8b18c60d11b852cd348ab1c747251980246f9679
Stefan Berger <stefanb@us.ibm.com>
2020-06-24 12:50:24 +0000

    LibOS: Print missing MAP_XYZ flags

--- LibOS/shim/src/shim_parser.c
+++ LibOS/shim/src/shim_parser.c
@@ -775,6 +775,56 @@ static void parse_mmap_flags(va_list* ap) {
     }
 #endif
 
+    if (flags & MAP_GROWSDOWN) {
+        PUTS("|MAP_GROWSDOWN");
+        flags &= ~MAP_GROWSDOWN;
+    }
+
+    if (flags & MAP_DENYWRITE) {
+        PUTS("|MAP_DENYWRITE");
+        flags &= ~MAP_DENYWRITE;
+    }
+
+    if (flags & MAP_EXECUTABLE) {
+        PUTS("|MAP_EXECUTABLE");
+        flags &= ~MAP_EXECUTABLE;
+    }
+
+    if (flags & MAP_LOCKED) {
+        PUTS("|MAP_LOCKED");
+        flags &= ~MAP_LOCKED;
+    }
+
+    if (flags & MAP_NORESERVE) {
+        PUTS("|MAP_NORESERVE");
+        flags &= ~MAP_NORESERVE;
+    }
+
+    if (flags & MAP_POPULATE) {
+        PUTS("|MAP_POPULATE");
+        flags &= ~MAP_POPULATE;
+    }
+
+    if (flags & MAP_NONBLOCK) {
+        PUTS("|MAP_NONBLOCK");
+        flags &= ~MAP_NONBLOCK;
+    }
+
+    if (flags & MAP_STACK) {
+        PUTS("|MAP_STACK");
+        flags &= ~MAP_STACK;
+    }
+
+    if (flags & MAP_HUGETLB) {
+        PUTS("|MAP_HUGETLB");
+        flags &= ~MAP_HUGETLB;
+    }
+
+    if (flags & MAP_SYNC) {
+        PUTS("|MAP_SYNC");
+        flags &= ~MAP_SYNC;
+    }
+
     if (flags)
         PRINTF("|0x%x", flags);
 }
