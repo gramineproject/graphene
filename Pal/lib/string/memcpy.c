619e92a74aa4281d4a2d7186ff69526963ae4c95
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2020-07-01 19:42:43 +0000

    fixup! [Pal/lib] Replace old memory-handling functions with simpler versions

--- Pal/lib/string/memcpy.c
+++ Pal/lib/string/memcpy.c
@@ -20,10 +20,12 @@ void* memcpy(void* restrict dst, const void* restrict src, size_t n) {
 #if defined(__x86_64__)
     /* "Beginning with processors based on Intel microarchitecture code name Ivy Bridge, REP string
      * operation using MOVSB and STOSB can provide both flexible and high-performance REP string
-     * operations for software in common situations like memory copy and set operations".
+     * operations for software in common situations like memory copy and set operations" (c)
+     * Intel 64 and IA-32 Architectures Optimization Reference Manual.
      *
      * memcpy() is heavily used in Linux-SGX PAL to copy data in/out of SGX enclave. Experiments
-     * with Redis 5.0 show perf improvement of using "rep movsb" at 3-5% for 4KB payloads. */
+     * with Redis 5.0 show perf improvement of using "rep movsb" at 3-5% for 4KB payloads over
+     * previous implementation taken from Glibc 2.23. */
     __asm__ volatile("rep movsb" : "+D" (d) : "c"(n), "S"(src) : "cc", "memory");
 #else
     for (; n; n--)
