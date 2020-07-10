5179ce417311634191cfb18f1f67f2fa374ec11c
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2020-07-10 14:56:59 -0700

    [Pal/Linux-SGX] Introduce manifest option "sgx.zero_heap_on_demand"
    Commit 647bca34 changed the old logic of "zero the whole heap during
    enclave initialization" to "zero the heap pages on demand". This led
    to better start-up times of enclaves (without any significant
    security drawbacks). However, this change has a trade-off: some
    applications (Redis in particular) have worse run-time performance.
    
    To allow users to choose between faster start-up time vs better
    run-time performance, this commit introduces "sgx.zero_heap_on_demand"
    manifest option. It is zero by default, meaning zero-out of whole
    heap during enclave initialization; i.e., by default enclaves take
    longer to start, but have better run-time performance.

--- Pal/src/host/Linux-SGX/pal_security.h
+++ Pal/src/host/Linux-SGX/pal_security.h
@@ -20,8 +20,9 @@ struct pal_sec {
     sgx_measurement_t  mr_signer;
     sgx_attributes_t   enclave_attributes;
 
-    /* remaining heap usable by application */
+    /* remaining heap usable by application and whether to zero it on demand/during init */
     PAL_PTR         heap_min, heap_max;
+    PAL_BOL         zero_heap_on_demand;
 
     /* executable name, addr and size */
     PAL_SEC_STR     exec_name;
