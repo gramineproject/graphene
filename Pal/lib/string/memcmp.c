bdcf29ba337f4e68e674a5ef04cc5a48d50b18cd
Michał Kowalczyk <mkow@invisiblethingslab.com>
2020-07-23 03:33:43 +0200

    [Pal] Simplify memset and clean up other string ops

--- Pal/lib/string/memcmp.c
+++ Pal/lib/string/memcmp.c
@@ -1,24 +1,16 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+#include <stdint.h>
+
 #include "api.h"
 
-/* Copyright © 2005-2014 Rich Felker, et al. */
-
-/* Permission is hereby granted, free of charge, to any person obtaining */
-/* a copy of this software and associated documentation files (the */
-/* "Software"), to deal in the Software without restriction, including */
-/* without limitation the rights to use, copy, modify, merge, publish, */
-/* distribute, sublicense, and/or sell copies of the Software, and to */
-/* permit persons to whom the Software is furnished to do so, subject to */
-/* the following conditions: */
-
-/* The above copyright notice and this permission notice shall be */
-/* included in all copies or substantial portions of the Software. */
-
-/* musl 1.1.24 */
-
-int memcmp(const void* vl, const void* vr, size_t n) {
-    const unsigned char* l=vl;
-    const unsigned char* r=vr;
-    for (; n && *l == *r; n--, l++, r++)
-        /*nop*/;
-    return n ? *l - *r : 0;
+int memcmp(const void* lhs, const void* rhs, size_t count) {
+    const unsigned char* l = lhs;
+    const unsigned char* r = rhs;
+    while (count && *l == *r) {
+        count--;
+        l++;
+        r++;
+    }
+    return count ? *l - *r : 0;
 }
--- Pal/lib/string/memcpy.c
+++ Pal/lib/string/memcpy.c
@@ -1,22 +1,11 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+#include <stdint.h>
+
 #include "api.h"
 
-/* Copyright © 2005-2014 Rich Felker, et al. */
-
-/* Permission is hereby granted, free of charge, to any person obtaining */
-/* a copy of this software and associated documentation files (the */
-/* "Software"), to deal in the Software without restriction, including */
-/* without limitation the rights to use, copy, modify, merge, publish, */
-/* distribute, sublicense, and/or sell copies of the Software, and to */
-/* permit persons to whom the Software is furnished to do so, subject to */
-/* the following conditions: */
-
-/* The above copyright notice and this permission notice shall be */
-/* included in all copies or substantial portions of the Software. */
-
-/* heavily based on musl 1.1.15/1.1.24 */
-
-void* memcpy(void* restrict dst, const void* restrict src, size_t n) {
-    char* d = dst;
+void* memcpy(void* restrict dest, const void* restrict src, size_t count) {
+    char* d = dest;
 #if defined(__x86_64__)
     /* "Beginning with processors based on Intel microarchitecture code name Ivy Bridge, REP string
      * operation using MOVSB and STOSB can provide both flexible and high-performance REP string
@@ -26,33 +15,31 @@ void* memcpy(void* restrict dst, const void* restrict src, size_t n) {
      * memcpy() is heavily used in Linux-SGX PAL to copy data in/out of SGX enclave. Experiments
      * with Redis 5.0 show perf improvement of using "rep movsb" at 3-5% for 4KB payloads over
      * previous implementation taken from Glibc 2.23. */
-    __asm__ volatile("rep movsb" : "+D" (d) : "c"(n), "S"(src) : "cc", "memory");
+    __asm__ volatile("rep movsb" : "+&D"(d), "+&c"(count) : "S"(src) : "cc", "memory");
 #else
     const char* s = src;
-    for (; n; n--)
+    while (count--)
         *d++ = *s++;
 #endif
-    return dst;
+    return dest;
 }
 
-void* memmove(void* dst, const void* src, size_t n) {
-    char* d = dst;
+void* memmove(void* dest, const void* src, size_t count) {
+    char* d = dest;
     const char* s = src;
 
     if (d == s)
         return d;
 
-    if (s + n <= d || d + n <= s)
-        return memcpy(d, s, n);
+    if (s + count <= d || d + count <= s)
+        return memcpy(d, s, count);
 
     if (d < s) {
-        for (; n; n--)
+        while (count--)
             *d++ = *s++;
     } else {
-        while (n) {
-            n--;
-            d[n] = s[n];
-        }
+        while (count--)
+            d[count] = s[count];
     }
-    return dst;
+    return dest;
 }
