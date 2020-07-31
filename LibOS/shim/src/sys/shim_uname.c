1f80a855c6cf3b4e41632b119fb90b9554f53430
Michał Kowalczyk <mkow@invisiblethingslab.com>
2020-07-31 03:34:57 +0200

    [LibOS] Fix uname syscall to not leave uninitialized user memory

--- LibOS/shim/src/sys/shim_uname.c
+++ LibOS/shim/src/sys/shim_uname.c
@@ -2,28 +2,31 @@
 /* Copyright (C) 2014 Stony Brook University */
 
 /*
- * shim_uname.c
- *
- * Implementation of system call "uname".
+ * Implementation of system call `uname`.
  */
 
 #include <errno.h>
-#include <shim_fs.h>
-#include <shim_handle.h>
-#include <shim_internal.h>
-#include <shim_table.h>
-#include <shim_utils.h>
 #include <sys/utsname.h>
 
-/* DP: Damned lies */
-static struct old_utsname graphene_uname = {.sysname  = "Linux",
-                                            .nodename = "localhost",
-                                            .release  = "3.10.0",
-                                            .version  = "1",
-                                            .machine  = "x86_64"};
+#include "api.h"
+#include "shim_internal.h"
+#include "shim_table.h"
 
-int shim_do_uname(struct old_utsname* buf) {
-    if (!buf || test_user_memory(buf, sizeof(*buf), true))
+static struct new_utsname graphene_uname = {
+    .sysname    = "Linux",
+    .nodename   = "localhost",
+    .release    = "3.10.0",
+    .version    = "1",
+#ifdef __x86_64__
+    .machine    = "x86_64",
+#else
+    #error "Not implemented"
+#endif
+    .domainname = "(none)", /* this seems to be the default on Linux */
+};
+
+int shim_do_uname(struct new_utsname* buf) {
+    if (!buf || test_user_memory(buf, sizeof(*buf), /*write=*/true))
         return -EFAULT;
 
     memcpy(buf, &graphene_uname, sizeof(graphene_uname));
