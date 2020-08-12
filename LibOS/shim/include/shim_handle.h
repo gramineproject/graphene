gramineproject/graphene

commit 2abb2f325e7431c4726d207a0a335e16f4db7f0e

[LibOS] Fix setting O_NONBLOCK flag in pipe2
Specifying O_NONBLOCK flag in the call to pipe2 applied it only to one
end of the pipe.

LibOS/shim/include/shim_handle.h
@@ -348,6 +348,9 @@ void flush_handle(struct shim_handle* hdl);
 void get_handle(struct shim_handle* hdl);
 void put_handle(struct shim_handle* hdl);
 
+/* Set handle in non-blocking mode. */
+int set_handle_nonblocking(struct shim_handle* hdl);
+
 /* file descriptor table */
 struct shim_fd_handle {
     FDTYPE vfd; /* virtual file descriptor */

LibOS/shim/src/sys/shim_fcntl.c
@@ -18,6 +18,26 @@
 #include <shim_thread.h>
 #include <shim_utils.h>
 
+#define FCNTL_SETFL_MASK (O_APPEND | O_NONBLOCK)
+
+static int _set_handle_flags(struct shim_handle* hdl, unsigned long arg) {
+    if (hdl->fs && hdl->fs->fs_ops && hdl->fs->fs_ops->setflags) {
+        int ret = hdl->fs->fs_ops->setflags(hdl, arg & FCNTL_SETFL_MASK);
+        if (ret < 0) {
+            return ret;
+        }
+    }
+    hdl->flags |= (hdl->flags & ~FCNTL_SETFL_MASK) | (arg & FCNTL_SETFL_MASK);
+    return 0;
+}
+
+int set_handle_nonblocking(struct shim_handle* hdl) {
+    lock(&hdl->lock);
+    int ret = _set_handle_flags(hdl, hdl->flags | O_NONBLOCK);
+    unlock(&hdl->lock);
+    return ret;
+}
+
 int shim_do_fcntl(int fd, int cmd, unsigned long arg) {
     struct shim_handle_map* handle_map = get_cur_handle_map(NULL);
     int flags;
@@ -118,14 +138,10 @@ int shim_do_fcntl(int fd, int cmd, unsigned long arg) {
          *   Linux this command can only change the O_APPEND, O_DIRECT,
          *   O_NOATIME, and O_NONBLOCK flags.
          */
-#define FCNTL_SETFL_MASK (O_APPEND | O_NONBLOCK)
         case F_SETFL:
             lock(&hdl->lock);
-            if (hdl->fs && hdl->fs->fs_ops && hdl->fs->fs_ops->setflags)
-                hdl->fs->fs_ops->setflags(hdl, arg & FCNTL_SETFL_MASK);
-            hdl->flags = (hdl->flags & ~FCNTL_SETFL_MASK) | (arg & FCNTL_SETFL_MASK);
+            ret = _set_handle_flags(hdl, arg);
             unlock(&hdl->lock);
-            ret = 0;
             break;
 
         /* Advisory locking

LibOS/shim/src/sys/shim_ioctl.c
@@ -328,10 +328,7 @@ int shim_do_ioctl(int fd, unsigned long cmd, unsigned long arg) {
             ret = ioctl_termios(hdl, cmd, arg);
             break;
         case FIONBIO:
-            if (hdl->fs && hdl->fs->fs_ops && hdl->fs->fs_ops->setflags)
-                hdl->fs->fs_ops->setflags(hdl, hdl->flags | O_NONBLOCK);
-            hdl->flags |= O_NONBLOCK;
-            ret = 0;
+            ret = set_handle_nonblocking(hdl);
             break;
         case FIONCLEX:
             hdl->flags &= ~FD_CLOEXEC;

LibOS/shim/src/sys/shim_pipe.c
@@ -20,7 +20,7 @@
 #include "shim_types.h"
 #include "shim_utils.h"
 
-static int create_pipes(PAL_HANDLE* srv, PAL_HANDLE* cli, int flags, char* name,
+static int create_pipes(struct shim_handle* srv, struct shim_handle* cli, int flags, char* name,
                         struct shim_qstr* qstr) {
     int ret = 0;
     char uri[PIPE_URI_SIZE];
@@ -28,11 +28,6 @@ static int create_pipes(PAL_HANDLE* srv, PAL_HANDLE* cli, int flags, char* name,
     PAL_HANDLE hdl0 = NULL;  /* server pipe (temporary, waits for connect from hdl2) */
     PAL_HANDLE hdl1 = NULL;  /* one pipe end (accepted connect from hdl2) */
     PAL_HANDLE hdl2 = NULL;  /* other pipe end (connects to hdl0 and talks to hdl1) */
-
-    if (flags & O_DIRECT) {
-        debug("create_pipes(): tried to create O_DIRECT pipe, which is not supported.\n");
-        flags &= ~O_DIRECT;
-    }
 
     if ((ret = create_pipe(name, uri, PIPE_URI_SIZE, &hdl0, qstr,
                            /*use_vmid_for_name=*/false)) < 0) {
@@ -52,8 +47,20 @@ static int create_pipes(PAL_HANDLE* srv, PAL_HANDLE* cli, int flags, char* name,
         goto out;
     }
 
-    *srv = hdl1;
-    *cli = hdl2;
+    PAL_HANDLE tmp = srv->pal_handle;
+    srv->pal_handle = hdl1;
+
+    if (flags & O_NONBLOCK) {
+        /* `cli` - `hdl2` - has this flag already set by the call to `DkStreamOpen`. */
+        ret = set_handle_nonblocking(srv);
+        if (ret < 0) {
+            /* Restore original handle, if any. */
+            srv->pal_handle = tmp;
+            goto out;
+        }
+    }
+
+    cli->pal_handle = hdl2;
     ret = 0;
 
 out:
@@ -78,6 +85,15 @@ static void undo_set_fd_handle(int fd) {
 
 int shim_do_pipe2(int* filedes, int flags) {
     int ret = 0;
+
+    if (flags & O_DIRECT) {
+        debug("shim_do_pipe2(): ignoring a not supported O_DIRECT flag\n");
+        flags &= ~O_DIRECT;
+    }
+
+    if (flags & ~(O_NONBLOCK | O_CLOEXEC)) {
+        return -EINVAL;
+    }
 
     if (!filedes || test_user_memory(filedes, 2 * sizeof(int), true))
         return -EFAULT;
@@ -106,8 +122,7 @@ int shim_do_pipe2(int* filedes, int flags) {
     hdl1->info.pipe.ready_for_ops = true;
     hdl2->info.pipe.ready_for_ops = true;
 
-    ret = create_pipes(&hdl1->pal_handle, &hdl2->pal_handle, flags, hdl1->info.pipe.name,
-                       &hdl1->uri);
+    ret = create_pipes(hdl1, hdl2, flags, hdl1->info.pipe.name, &hdl1->uri);
     if (ret < 0)
         goto out;
 
@@ -190,8 +205,8 @@ int shim_do_socketpair(int domain, int type, int protocol, int* sv) {
     sock2->protocol   = protocol;
     sock2->sock_state = SOCK_CONNECTED;
 
-    ret = create_pipes(&hdl1->pal_handle, &hdl2->pal_handle, type & SOCK_NONBLOCK ? O_NONBLOCK : 0,
-                       sock1->addr.un.name, &hdl1->uri);
+    ret = create_pipes(hdl1, hdl2, type & SOCK_NONBLOCK ? O_NONBLOCK : 0, sock1->addr.un.name,
+                       &hdl1->uri);
     if (ret < 0)
         goto out;
 
@@ -312,8 +327,7 @@ int shim_do_mknodat(int dirfd, const char *pathname, mode_t mode, dev_t dev) {
 
     /* FIFO pipes are created in blocking mode; they will be changed to non-blocking if open()'ed
      * in non-blocking mode later (see fifo_open) */
-    ret = create_pipes(&hdl1->pal_handle, &hdl2->pal_handle, /*flags=*/0, hdl1->info.pipe.name,
-                       &hdl1->uri);
+    ret = create_pipes(hdl1, hdl2, /*flags=*/0, hdl1->info.pipe.name, &hdl1->uri);
     if (ret < 0)
         goto out;
 

LibOS/shim/test/regression/.gitignore
@@ -49,6 +49,7 @@
 /multi_pthread
 /openmp
 /pipe
+/pipe_nonblocking
 /poll
 /poll_closed_fd
 /poll_many_types

LibOS/shim/test/regression/Makefile
@@ -45,6 +45,7 @@ c_executables = \
 	multi_pthread \
 	openmp \
 	pipe \
+	pipe_nonblocking \
 	poll \
 	poll_closed_fd \
 	poll_many_types \

LibOS/shim/test/regression/pipe_nonblocking.c
@@ -1,0 +1,42 @@
+#define _GNU_SOURCE
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void) {
+    int p[2];
+
+    if (pipe2(p, O_NONBLOCK | O_CLOEXEC) < 0) {
+        err(1, "pipe2");
+    }
+
+    ssize_t ret = write(p[1], "a", 1);
+    if (ret < 0) {
+        err(1, "write");
+    } else if (ret != 1) {
+        errx(1, "invalid return value from write: %zd\n", ret);
+    }
+
+    char c;
+    ret = read(p[0], &c, 1);
+    if (ret < 0) {
+        err(1, "read");
+    } else if (ret != 1) {
+        errx(1, "invalid return value from read: %zd\n", ret);
+    }
+
+    ret = read(p[0], &c, 1);
+    if (ret > 0) {
+        errx(1, "read returned unexpected data: %zd\n", ret);
+    } else if (ret == 0) {
+        errx(1, "read returned 0 instead of EAGAIN\n");
+    } else if (errno != EAGAIN && errno != EWOULDBLOCK) {
+        err(1, "unexpected read failure");
+    }
+
+    puts("TEST OK");
+    return 0;
+}
+

LibOS/shim/test/regression/test_libos.py
@@ -605,6 +605,10 @@ class TC_80_Socket(RegressionTestCase):
         stdout, _ = self.run_binary(['pipe'], timeout=60)
         self.assertIn('read on pipe: Hello from write end of pipe!', stdout)
 
+    def test_091_pipe_nonblocking(self):
+        stdout, _ = self.run_binary(['pipe_nonblocking'])
+        self.assertIn('TEST OK', stdout)
+
     def test_095_mkfifo(self):
         stdout, _ = self.run_binary(['mkfifo'], timeout=60)
         self.assertIn('read on FIFO: Hello from write end of FIFO!', stdout)

commit ade83d1fc5bf2bafa68b8ff73310e9de8b6f9037

Fix a bug introduced in 2abb2f325e7431c4726d207a0a335e16f4db7f0e

LibOS/shim/include/shim_handle.h
@@ -348,7 +348,7 @@ void flush_handle(struct shim_handle* hdl);
 void get_handle(struct shim_handle* hdl);
 void put_handle(struct shim_handle* hdl);
 
-/* Set handle in non-blocking mode. */
+/* Set handle to non-blocking mode. */
 int set_handle_nonblocking(struct shim_handle* hdl);
 
 /* file descriptor table */

LibOS/shim/src/sys/shim_fcntl.c
@@ -27,7 +27,7 @@ static int _set_handle_flags(struct shim_handle* hdl, unsigned long arg) {
             return ret;
         }
     }
-    hdl->flags |= (hdl->flags & ~FCNTL_SETFL_MASK) | (arg & FCNTL_SETFL_MASK);
+    hdl->flags = (hdl->flags & ~FCNTL_SETFL_MASK) | (arg & FCNTL_SETFL_MASK);
     return 0;
 }
 