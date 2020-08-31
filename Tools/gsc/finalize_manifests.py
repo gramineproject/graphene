3ca6f36c758460b9d5b6fc6b0ce977f27e02b316
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2020-08-31 22:48:40 +0000

    fixup! [GSC] Exclude /graphene/signer/* from generated list of trusted files

--- Tools/gsc/finalize_manifests.py
+++ Tools/gsc/finalize_manifests.py
@@ -18,10 +18,10 @@ def generate_trusted_files(root_dir):
     cwd = os.getcwd() if os.getcwd() != '/' else ''
     # Exclude files and paths from list of trusted files
     excluded_paths_regex = (r'^/('
-                                r'|graphene/signer/.*'
                                 r'boot/.*'
                                 r'|dev/.*'
                                 r'|etc/rc(\d|.)\.d/.*'
+                                r'|graphene/signer/.*'
                                 r'|proc/.*'
                                 r'|sys/.*'
                                 r'|var/.*)'
