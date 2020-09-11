307e9fb1fbd5773f1d442c42964c455cf818c567
Michał Kowalczyk <mkow@invisiblethingslab.com>
2020-09-11 14:22:20 +0200

    Fix Bash scripts and remove unused ones

--- Examples/bash/scripts/bash_test.sh
+++ Examples/bash/scripts/bash_test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 times=$1
-[ $times -gt 0 2> /dev/null ] || times=300
+[ "$times" -gt 0 ] 2>/dev/null || times=300
 
-for (( c=1; c<=$times; c++ ))
+for (( c=1; c<=times; c++ ))
 do
 	echo "hello $c"
 	cp somefile testdir/somefile
