3d10b9c30c577e23bd8648b207e3a9c9580ec88a
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2020-09-29 05:40:56 -0700

    [Pal/Linux-SGX] Save XSAVE area of normal app context on sync signals
    Previously, when a synchronous signal like SIGFPE/SIGILL happened in
    the application (e.g., due to div-by-zero or SGX-prohibited SYSCALL
    instruction), Linux-SGX assembly routine incorrectly saved the XSAVE
    area (containing among other things FP/SSE/AVX registers) of the
    signal context. This commit fixes this bug by extracting the XSAVE
    area of the normal app context, i.e., XSAVE of SSA 0.
    
    New LibOS regression test is added to test this bug fix. The test
    is x64-only and assumes an SSE2 enabled CPU.

---
+++ LibOS/shim/test/regression/sighandler_divbyzero.c
@@ -0,0 +1,61 @@
+#define _GNU_SOURCE
+#include <emmintrin.h> // Intel SSE2 (XMM) intrinsics
+#include <errno.h>
+#include <signal.h>
+#include <stdatomic.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ucontext.h>
+
+static atomic_int sigfpe_ctr = 0;
+
+static void sigfpe_handler(int signum, siginfo_t* si, void* uc) {
+    printf("Got signal %d\n", signum);
+    if (signum == SIGFPE) {
+        ((ucontext_t*)uc)->uc_mcontext.gregs[REG_RBX] = 1; /* fix divisor */
+        sigfpe_ctr++;
+    }
+}
+
+int main(int argc, char** argv) {
+    int ret;
+
+    const struct sigaction act = {
+        .sa_sigaction = sigfpe_handler,
+    };
+
+    ret = sigaction(SIGFPE, &act, NULL);
+    if (ret < 0) {
+        fprintf(stderr, "sigaction failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    /* populate array with 0,1,2,3... via argc (this test expects no command-line options and thus
+     * argc == 1); this is to prevent the compiler from optimizing out the XMM logic */
+    int16_t a[8];
+    for (int i = 0; i < 8; i++)
+        a[i] = argc - 1 + i;
+    __m128i xmm_reg = _mm_load_si128((__m128i*)a);
+    __asm__ volatile("" ::: "memory");
+
+    __asm__ volatile("movq $1, %%rax\n"
+                     "cqo\n"
+                     "movq $0, %%rbx\n"
+                     "divq %%rbx\n"
+                     ::: "rax", "rbx", "rdx", "cc", "memory");
+
+    if (_mm_extract_epi16(xmm_reg, 0) != 0 || _mm_extract_epi16(xmm_reg, 1) != 1 ||
+        _mm_extract_epi16(xmm_reg, 2) != 2 || _mm_extract_epi16(xmm_reg, 3) != 3 ||
+        _mm_extract_epi16(xmm_reg, 4) != 4 || _mm_extract_epi16(xmm_reg, 5) != 5 ||
+        _mm_extract_epi16(xmm_reg, 6) != 6 || _mm_extract_epi16(xmm_reg, 7) != 7) {
+        fprintf(stderr, "XSAVE state (XMM registers' values) was lost!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    __asm__ volatile("" ::: "memory");
+    printf("Got %d SIGFPE signal(s)\n", sigfpe_ctr);
+    puts("TEST OK");
+    exit(EXIT_SUCCESS);
+}
