6f62fcb9cc6b835b17245f2e2c8b539488049f1d
Juro Bystricky <juro.bystricky@intel.com>
2020-10-10 16:23:49 -0700

    [LibOS] Use correct v5.8 x86 syscall numbers
    Fixes #1878
    
    When compiling against v5.8 (or later) kernel headers, we encounter
    compilation errors such as:
    
      In file included from /usr/include/asm/unistd.h:13,
                       from shim_parser.c:11:
      shim_parser.c:408:10: error: array index in initializer exceeds array bounds
               [__NR_io_uring_setup]    = {.slow = 0, .parser = {NULL}},
    
    The remedy is to extend "syscall_parser_table" by modyfing the
    definition of LIBOS_SYSCALL_BOUND so the offending system calls fit in the table.
    
    While at it, also fix the actual values for syscall numbers in "shim_syscalls.h"
    (The numbers are not sequential, there is an intentional gap between the numers)

--- LibOS/shim/include/shim_defs.h
+++ LibOS/shim/include/shim_defs.h
@@ -1,6 +1,10 @@
 #ifndef _SHIM_DEFS_H_
 #define _SHIM_DEFS_H_
 
+#if defined(__i386__) || defined(__x86_64__)
+#include "shim_syscalls.h"
+#endif
+
 #define DEFAULT_HEAP_MIN_SIZE  (256 * 1024 * 1024) /* 256MB */
 #define DEFAULT_MEM_MAX_NPAGES (1024 * 1024)       /* 4GB */
 #define DEFAULT_BRK_MAX_SIZE   (256 * 1024)        /* 256KB */
@@ -19,7 +23,7 @@
 #define REQUIRED_ELF_AUXV_SPACE 16 /* extra memory space (in bytes) */
 
 #if defined(__i386__) || defined(__x86_64__)
-#define LIBOS_SYSCALL_BOUND (340 + 1)
+#define LIBOS_SYSCALL_BOUND __NR_syscalls
 #endif
 
 #endif /* _SHIM_DEFS_H_ */
