8551fcdce87066024975acb3eb4aecfc11d7ddd7
Paweł Marczewski <pawel@invisiblethingslab.com>
2020-10-12 11:48:39 +0200

    fixup! Add a regression test for GDB integration

--- LibOS/shim/test/regression/debug.c
+++ LibOS/shim/test/regression/debug.c
@@ -4,7 +4,7 @@ __attribute__((noinline)) static void func(void) {
     printf("hello\n");
     fflush(stdout);
     /* Prevent tail call, so that we get a stack trace including func(). */
-    __asm__("");
+    __asm__ volatile("");
 }
 
 int main(void) {
