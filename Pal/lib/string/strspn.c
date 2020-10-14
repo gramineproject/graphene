8339ae0fdf2acc9a45fcb61dccf417c5e444d550
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2020-10-14 05:56:54 -0700

    [Pal/lib] Add strncmp() and strspn() string manipulation funcs
    These functions are required by the TOML C source code, which will be
    embedded into Graphene in a future commit.

---
+++ Pal/lib/string/strspn.c
@@ -0,0 +1,42 @@
+/* Copyright © 2005-2020 Rich Felker, et al.
+
+   Permission is hereby granted, free of charge, to any person obtaining
+   a copy of this software and associated documentation files (the
+   "Software"), to deal in the Software without restriction, including
+   without limitation the rights to use, copy, modify, merge, publish,
+   distribute, sublicense, and/or sell copies of the Software, and to
+   permit persons to whom the Software is furnished to do so, subject to
+   the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
+#include "api.h"
+
+#define BITOP(a,b,op) \
+ ((a)[(size_t)(b)/(8*sizeof *(a))] op (size_t)1<<((size_t)(b)%(8*sizeof *(a))))
+
+size_t strspn(const char* s, const char* c) {
+    const char *a = s;
+    size_t byteset[32/sizeof(size_t)] = { 0 };
+
+    if (!c[0])
+        return 0;
+
+    if (!c[1]) {
+        for (; *s == *c; s++);
+        return s - a;
+    }
+
+    for (; *c && BITOP(byteset, *(unsigned char*)c, |=); c++);
+    for (; *s && BITOP(byteset, *(unsigned char*)s, &); s++);
+    return s - a;
+}
