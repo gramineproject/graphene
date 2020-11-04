gramineproject/graphene

commit 50ca6c4a2284c9d06b48b6b3e8c2ef9eb10f0f90

[LibOS] Deliver signal to only one thread
Previously Graphene delivered each signal to all threads instead of
one. This was additionally abused to quit the application. Now each
signal is delivered to only one arbitrary thread. This commit
additionally cleans the signal and exiting code.

LibOS/shim/include/shim_thread.h
@@ -16,7 +16,6 @@
 struct shim_handle;
 struct shim_fd_map;
 struct shim_dentry;
-struct shim_signal_log;
 
 #define WAKE_QUEUE_TAIL ((void*)1)
 /* If next is NULL, then this node is not on any queue.
@@ -32,6 +31,20 @@ struct shim_signal_handles {
     struct __kernel_sigaction actions[NUM_SIGS];
     struct shim_lock lock;
     REFTYPE ref_count;
+};
+
+/* For more info see: man signal(7) */
+#define MAX_SIGNAL_LOG 32
+
+struct shim_rt_signal_queue {
+    uint64_t put_idx;
+    uint64_t get_idx;
+    struct shim_signal* queue[MAX_SIGNAL_LOG];
+};
+
+struct shim_signal_queue {
+    struct shim_signal* standard_signals[SIGRTMIN - 1];
+    struct shim_rt_signal_queue rt_signal_queues[NUM_SIGS - SIGRTMIN + 1];
 };
 
 DEFINE_LIST(shim_thread);
@@ -70,9 +83,11 @@ struct shim_thread {
     /* signal handling */
     __sigset_t signal_mask;
     struct shim_signal_handles* signal_handles;
-    struct atomic_int has_signal;
-    struct shim_signal_log * signal_logs;
-    bool suspend_on_signal;
+    struct shim_signal_queue signal_queue;
+    /* For the field below, see the explanation in "LibOS/shim/src/bookkeep/shim_signal.c" near
+     * `process_pending_signals_cnt`. */
+    uint64_t pending_signals;
+    bool signal_handled;
     stack_t signal_altstack;
 
     /* futex robust list */
@@ -87,6 +102,7 @@ struct shim_thread {
     int term_signal; // Store the terminating signal, if any; needed for
                      // wait() and friends
     bool is_alive;
+    bool time_to_die;
 
     PAL_HANDLE child_exit_event;
     LISTP_TYPE(shim_thread) exited_children;
@@ -144,14 +160,6 @@ struct shim_thread* get_cur_thread (void) {
 
 static inline
 __attribute__((always_inline))
-bool cur_thread_is_alive (void)
-{
-    struct shim_thread * thread = get_cur_thread();
-    return thread ? thread->is_alive : false;
-}
-
-static inline
-__attribute__((always_inline))
 void set_cur_thread (struct shim_thread * thread)
 {
     shim_tcb_t * tcb = shim_get_tcb();
@@ -167,11 +175,6 @@ void set_cur_thread (struct shim_thread * thread)
         tcb->tp = thread;
         thread->shim_tcb = tcb;
         tid = thread->tid;
-
-        if (!is_internal(thread) && !thread->signal_logs) {
-            thread->signal_logs = signal_logs_alloc();
-            assert(thread->signal_logs); /* FIXME on ENOMEM */
-        }
     } else if (tcb->tp) {
         put_thread(tcb->tp);
         tcb->tp = NULL;
@@ -280,10 +283,10 @@ void add_thread (struct shim_thread * thread);
 void del_thread (struct shim_thread * thread);
 
 void cleanup_thread(IDTYPE caller, void* thread);
-int check_last_thread(struct shim_thread* self);
+bool mark_self_dead(void);
+bool check_last_thread(void);
 
-int walk_thread_list (int (*callback) (struct shim_thread *, void *, bool *),
-                      void * arg);
+int walk_thread_list(int (*callback)(struct shim_thread*, void*), void* arg, bool one_shot);
 
 void dump_threads(void);
 
@@ -316,8 +319,10 @@ void set_handle_map (struct shim_thread * thread,
     thread->handle_map = map;
 }
 
-int thread_exit(struct shim_thread* self, bool send_ipc);
-noreturn void thread_or_process_exit(int error_code, int term_signal);
+int thread_destroy(struct shim_thread* self, bool send_ipc);
+bool kill_other_threads(void);
+noreturn void thread_exit(int error_code, int term_signal);
+noreturn void process_exit(int error_code, int term_signal);
 
 void release_robust_list(struct robust_list_head* head);
 

LibOS/shim/src/sys/shim_exec.c
@@ -183,6 +183,7 @@ static BEGIN_MIGRATION_DEF(execve, struct shim_thread* thread, struct shim_proce
     DEFINE_MIGRATE(process, proc, sizeof(struct shim_process));
     DEFINE_MIGRATE(all_mounts, NULL, 0);
     DEFINE_MIGRATE(running_thread, thread, sizeof(struct shim_thread));
+    DEFINE_MIGRATE(pending_signals, NULL, 0);
     DEFINE_MIGRATE(handle_map, thread->handle_map, sizeof(struct shim_handle_map));
     DEFINE_MIGRATE(migratable, NULL, 0);
     DEFINE_MIGRATE(environ, envp, 0);
@@ -366,8 +367,20 @@ reopen:
         }
     }
 
-    bool use_same_process = check_last_thread(cur_thread) == 0;
-    if (use_same_process && !strcmp_static(PAL_CB(host_type), "Linux-SGX")) {
+    /* If `execve` is invoked concurrently by multiple threads, let only one succeed. */
+    static unsigned int first = 0;
+    if (__atomic_exchange_n(&first, 1, __ATOMIC_RELAXED) != 0) {
+        /* Just exit current thread. */
+        thread_exit(/*error_code=*/0, /*term_signal=*/0);
+    }
+    bool threads_killed = kill_other_threads();
+
+    /* All other threads are dead. Restoring initial value in case we stay inside same process
+     * instance and call execve again. */
+    __atomic_store_n(&first, 0, __ATOMIC_RELAXED);
+
+    bool use_same_process = true;
+    if (!strcmp_static(PAL_CB(host_type), "Linux-SGX")) {
         /* for SGX PALs, can use same process only if it is the same executable (because a different
          * executable has a different measurement and thus requires a new enclave); this special
          * case is to correctly handle e.g. Bash process replacing itself */
@@ -380,7 +393,13 @@ reopen:
 
     if (use_same_process) {
         debug("execve() in the same process\n");
-        return shim_do_execve_rtld(exec, argv, envp);
+        ret = shim_do_execve_rtld(exec, argv, envp);
+        if (threads_killed) {
+            /* We have killed some threads and execve failed internally. User app might now be in
+             * undefined state, we would better blow everything up. */
+            process_exit(ENOTRECOVERABLE, 0);
+        }
+        return ret;
     }
     debug("execve() in a new process\n");
 
@@ -432,6 +451,11 @@ reopen:
         /* execve failed, so reanimate this thread as if nothing happened */
         cur_thread->in_vm = true;
         unlock(&cur_thread->lock);
+        if (threads_killed) {
+            /* We have killed some threads and execve failed internally. User app might now be in
+             * undefined state, we would better blow everything up. */
+            process_exit(ENOTRECOVERABLE, 0);
+        }
         return ret;
     }
 

LibOS/shim/src/sys/shim_exit.c
@@ -20,94 +20,83 @@
  * Implementation of system call "exit" and "exit_group".
  */
 
-#include <shim_internal.h>
-#include <shim_table.h>
-#include <shim_thread.h>
-#include <shim_fs.h>
-#include <shim_handle.h>
-#include <shim_ipc.h>
-#include <shim_utils.h>
-#include <shim_checkpoint.h>
+#include "pal.h"
+#include "pal_error.h"
 
-#include <pal.h>
-#include <pal_error.h>
+#include "shim_handle.h"
+#include "shim_internal.h"
+#include "shim_ipc.h"
+#include "shim_table.h"
+#include "shim_thread.h"
+#include "shim_utils.h"
 
-#include <errno.h>
-#include <sys/syscall.h>
-#include <sys/mman.h>
-#include <linux/futex.h>
-
-void release_robust_list (struct robust_list_head * head);
-
-int thread_exit(struct shim_thread* self, bool send_ipc) {
+int thread_destroy(struct shim_thread* thread, bool send_ipc) {
     bool sent_exit_msg = false;
 
     /* Chia-Che: Broadcast exit message as early as possible,
        so other process can start early on responding. */
-    if (self->in_vm && send_ipc) {
-        ipc_cld_exit_send(self->ppid, self->tid, self->exit_code, self->term_signal);
+    if (thread->in_vm && send_ipc) {
+        ipc_cld_exit_send(thread->ppid, thread->tid, thread->exit_code, thread->term_signal);
         sent_exit_msg = true;
     }
 
-    lock(&self->lock);
+    lock(&thread->lock);
 
-    if (!self->is_alive) {
-        debug("thread %d is dead\n", self->tid);
-    out:
-        unlock(&self->lock);
+    if (!thread->is_alive || is_internal(thread)) {
+        unlock(&thread->lock);
         return 0;
     }
 
-    int exit_code = self->exit_code;
-
-    if (!self->in_vm) {
-        /* thread is in another process, we cannot rely on Async Helper thread to clean it */
-        self->is_alive = false;
+    if (!thread->in_vm) {
+        /* We need to mark remote threads as dead manually here. */
+        thread->is_alive = false;
     }
 
-    if (is_internal(self))
-        goto out;
+    int exit_code = thread->exit_code;
 
-    struct shim_handle_map * handle_map = self->handle_map;
-    struct shim_handle * exec = self->exec;
-    struct shim_thread * parent = self->parent;
-    self->handle_map = NULL;
-    self->exec = NULL;
+    struct shim_handle_map * handle_map = thread->handle_map;
+    struct shim_handle * exec = thread->exec;
+    struct shim_thread * parent = thread->parent;
+    thread->handle_map = NULL;
+    thread->exec = NULL;
 
     if (parent) {
-        assert(parent != self);
+        assert(parent != thread);
         assert(parent->child_exit_event);
         debug("thread exits, notifying thread %d\n", parent->tid);
 
         lock(&parent->lock);
-        LISTP_DEL_INIT(self, &parent->children, siblings);
-        LISTP_ADD_TAIL(self, &parent->exited_children, siblings);
+        LISTP_DEL_INIT(thread, &parent->children, siblings);
+        LISTP_ADD_TAIL(thread, &parent->exited_children, siblings);
+        unlock(&parent->lock);
 
-        if (!self->in_vm) {
+        if (!thread->in_vm) {
             debug("deliver SIGCHLD (thread = %d, exitval = %d)\n",
-                  self->tid, exit_code);
+                  thread->tid, exit_code);
 
             siginfo_t info;
             memset(&info, 0, sizeof(siginfo_t));
             info.si_signo = SIGCHLD;
-            info.si_pid   = self->tid;
-            info.si_uid   = self->uid;
+            info.si_pid   = thread->tid;
+            info.si_uid   = thread->uid;
             info.si_status = (exit_code & 0xff) << 8;
 
-            append_signal(parent, SIGCHLD, &info, true);
+            if (append_signal(parent, &info) >= 0) {
+                thread_wakeup(thread);
+                DkThreadResume(thread->pal_handle);
+            }
         }
-        unlock(&parent->lock);
 
         DkEventSet(parent->child_exit_event);
     } else if (!sent_exit_msg) {
         debug("parent not here, need to tell another process\n");
-        ipc_cld_exit_send(self->ppid, self->tid, self->exit_code, self->term_signal);
+        ipc_cld_exit_send(thread->ppid, thread->tid, thread->exit_code, thread->term_signal);
     }
 
-    struct robust_list_head* robust_list = self->robust_list;
-    self->robust_list = NULL;
+    struct robust_list_head* robust_list = thread->robust_list;
+    thread->robust_list = NULL;
 
-    unlock(&self->lock);
+    unlock(&thread->lock);
 
     if (handle_map)
         put_handle_map(handle_map);
@@ -118,21 +107,41 @@ int thread_exit(struct shim_thread* self, bool send_ipc) {
     if (robust_list)
         release_robust_list(robust_list);
 
-    DkEventSet(self->exit_event);
+    DkEventSet(thread->exit_event);
     return 0;
 }
 
-/* note that term_signal argument may contain WCOREDUMP bit (0x80) */
-noreturn void thread_or_process_exit(int error_code, int term_signal) {
-    struct shim_thread * cur_thread = get_cur_thread();
+static noreturn void libos_exit(int error_code, int term_signal) {
+    struct shim_thread* async_thread = terminate_async_helper();
+    if (async_thread) {
+        /* TODO: wait for the thread to exit in host.
+         * This is tracked by the following issue.
+         * https://github.com/oscarlab/graphene/issues/440
+         */
+        put_thread(async_thread);
+    }
+
+    struct shim_thread* ipc_thread = terminate_ipc_helper();
+    if (ipc_thread) {
+        /* TODO: wait for the thread to exit in host.
+         * This is tracked by the following issue.
+         * https://github.com/oscarlab/graphene/issues/440
+         */
+        put_thread(ipc_thread);
+    }
+
+    shim_clean_and_exit(term_signal ? term_signal : error_code);
+}
+
+noreturn void thread_exit(int error_code, int term_signal) {
+    struct shim_thread* cur_thread = get_cur_thread();
 
     cur_thread->exit_code = -error_code;
     cur_thread->term_signal = term_signal;
 
-    if (cur_thread->in_vm)
-        thread_exit(cur_thread, true);
+    thread_destroy(cur_thread, true);
 
-    if (check_last_thread(cur_thread)) {
+    if (!mark_self_dead()) {
         /* ask Async Helper thread to cleanup this thread */
         cur_thread->clear_child_tid_pal = 1; /* any non-zero value suffices */
         int64_t ret = install_async_event(NULL, 0, &cleanup_thread, cur_thread);
@@ -145,60 +154,85 @@ noreturn void thread_or_process_exit(int error_code, int term_signal) {
         DkThreadExit(&cur_thread->clear_child_tid_pal);
     }
 
-    struct shim_thread * async_thread = terminate_async_helper();
-    if (async_thread)
-        /* TODO: wait for the thread to exit in host.
-         * This is tracked by the following issue.
-         * https://github.com/oscarlab/graphene/issues/440
-         */
-        put_thread(async_thread); /* free resources of the thread */
-
-    struct shim_thread * ipc_thread = terminate_ipc_helper();
-    if (ipc_thread)
-        /* TODO: wait for the thread to exit in host.
-         * This is tracked by the following issue.
-         * https://github.com/oscarlab/graphene/issues/440
-         */
-        put_thread(ipc_thread); /* free resources of the thread */
-
-    shim_clean_and_exit(term_signal ? term_signal : error_code);
+    /* At this point other threads might be still in the middle of an exit routine, but we don't
+     * care since the below will call `exit_group` eventually. */
+    libos_exit(error_code, term_signal);
 }
 
-noreturn int shim_do_exit_group (int error_code)
-{
-    struct shim_thread * cur_thread = get_cur_thread();
-    assert(!is_internal(cur_thread));
-
-    /* If exit_group() is invoked multiple times, only a single invocation proceeds past this
-     * point. Kill signals are delivered asynchronously, which will eventually kick the execution
-     * out of this loop.*/
-    static struct atomic_int first = ATOMIC_INIT(0);
-    if (!atomic_cmpxchg(&first, 0, 1)) {
-        while (1)
-            DkThreadYieldExecution();
+static int mark_thread_to_die(struct shim_thread* thread, void* arg) {
+    if (thread == (struct shim_thread*)arg) {
+        return 0;
     }
+
+    bool need_wakeup = false;
+
+    lock(&thread->lock);
+    if (!thread->time_to_die) {
+        need_wakeup = true;
+    }
+    thread->time_to_die = true;
+    unlock(&thread->lock);
+
+    /* Now let's kick `thread`, so that it notices (in `__handle_signals`) the flag `time_to_die`
+     * set above (but only if we really set that flag). */
+    if (need_wakeup) {
+        thread_wakeup(thread);
+        DkThreadResume(thread->pal_handle);
+    }
+    return 1;
+}
+
+bool kill_other_threads(void) {
+    bool killed = false;
+    /* Tell other threads to exit. Since `mark_thread_to_die` never returns an error, this call
+     * cannot fail. */
+    if (walk_thread_list(mark_thread_to_die, get_cur_thread(), /*one_shot=*/false) != -ESRCH) {
+        killed = true;
+    }
+    DkThreadYieldExecution();
+
+    /* Wait for all other threads to exit. */
+    while (!check_last_thread()) {
+        /* Tell other threads to exit again - the previous announcement could have been missed by
+         * threads that were just being created. */
+        if (walk_thread_list(mark_thread_to_die, get_cur_thread(), /*one_shot=*/false) != -ESRCH) {
+            killed = true;
+        }
+        DkThreadYieldExecution();
+    }
+
+    return killed;
+}
+
+noreturn void process_exit(int error_code, int term_signal) {
+    /* If process_exit is invoked multiple times, only a single invocation proceeds past this
+     * point. */
+    static int first = 0;
+    if (__atomic_exchange_n(&first, 1, __ATOMIC_RELAXED) != 0) {
+        /* Just exit current thread. */
+        thread_exit(error_code, term_signal);
+    }
+
+    (void)kill_other_threads();
+
+    /* Now quit our thread. Since we are the last one, this will exit the whole LibOS. */
+    thread_exit(error_code, term_signal);
+}
+
+noreturn int shim_do_exit_group(int error_code) {
+    assert(!is_internal(get_cur_thread()));
 
     if (debug_handle)
         sysparser_printf("---- shim_exit_group (returning %d)\n", error_code);
 
-    debug("now kill other threads in the process\n");
-    do_kill_proc(cur_thread->tgid, cur_thread->tgid, SIGKILL, false);
-    while (check_last_thread(cur_thread)) {
-        DkThreadYieldExecution();
-    }
-
-    debug("now exit the process\n");
-    thread_or_process_exit(error_code, 0);
+    process_exit(error_code, 0);
 }
 
-noreturn int shim_do_exit (int error_code)
-{
-    struct shim_thread * cur_thread = get_cur_thread();
-    __UNUSED(cur_thread);
-    assert(!is_internal(cur_thread));
+noreturn int shim_do_exit(int error_code) {
+    assert(!is_internal(get_cur_thread()));
 
     if (debug_handle)
         sysparser_printf("---- shim_exit (returning %d)\n", error_code);
 
-    thread_or_process_exit(error_code, 0);
+    thread_exit(error_code, 0);
 }

LibOS/shim/test/ltp/ltp.cfg
@@ -2117,12 +2117,6 @@ skip = yes
 [rmdir05]
 skip = yes
 
-[rt_sigaction02]
-skip = yes
-
-[rt_sigprocmask01]
-skip = yes
-
 [rt_sigprocmask02]
 skip = yes
 
@@ -2713,9 +2707,6 @@ timeout = 40
 skip = yes
 
 [signal01]
-skip = yes
-
-[signal06]
 skip = yes
 
 [signalfd01]

LibOS/shim/include/arch/x86_64/shim_types-arch.h
@@ -7,7 +7,7 @@
 
 /* asm/signal.h */
 #define NUM_SIGS            64
-#define NUM_KNOWN_SIGS      32
+#define SIGRTMIN            32
 
 typedef struct {
     unsigned long __val[NUM_SIGS / (8 * sizeof(unsigned long))];

LibOS/shim/include/shim_internal.h
@@ -165,7 +165,7 @@ static inline PAL_HANDLE __open_shim_stdio (void)
     do { debug("%s (" __FILE__ ":%d)\n", __func__, __LINE__); } while (0)
 
 /* definition for syscall table */
-void handle_signal (void);
+void handle_signals(void);
 long convert_pal_errno (long err);
 void syscall_wrapper(void);
 void syscall_wrapper_after_syscalldb(void);
@@ -187,11 +187,10 @@ static inline int64_t get_cur_preempt (void) {
         SHIM_ARG_TYPE ret = 0;                              \
         int64_t preempt = get_cur_preempt();                \
         __UNUSED(preempt);                                  \
-        /* handle_signal(); */                              \
         /* check_stack_hook(); */
 
 #define END_SHIM(name)                                      \
-        handle_signal();                                    \
+        handle_signals();                                   \
         assert(preempt == get_cur_preempt());               \
         return ret;                                         \
     }
@@ -466,7 +465,7 @@ static inline void __enable_preempt (shim_tcb_t * tcb)
     //debug("enable preempt: %d\n", preempt);
 }
 
-void __handle_signal (shim_tcb_t * tcb, int sig);
+void __handle_signals(shim_tcb_t* tcb);
 
 static inline void enable_preempt (shim_tcb_t * tcb)
 {
@@ -478,7 +477,7 @@ static inline void enable_preempt (shim_tcb_t * tcb)
         return;
 
     if (preempt == 1)
-        __handle_signal(tcb, 0);
+        __handle_signals(tcb);
 
     __enable_preempt(tcb);
 }

LibOS/shim/include/shim_signal.h
@@ -97,33 +97,7 @@ struct shim_signal {
     PAL_CONTEXT * pal_context;
 };
 
-struct shim_signal_log;
-struct shim_signal_log* signal_logs_alloc(void);
-void signal_logs_free(struct shim_signal_log* signal_log);
-bool signal_logs_pending(const struct shim_signal_log* signal_log, int sig);
-
-extern const char * const siglist[NUM_KNOWN_SIGS + 1];
-
-static_always_inline
-const char * signal_name (int sig)
-{
-    if (sig <= NUM_KNOWN_SIGS)
-        return siglist[sig];
-
-    if (sig >= NUM_SIGS)
-        return "BAD SIGNAL";
-
-    char * str = __alloca(6);
-
-    str[0] = 'S';
-    str[1] = 'I';
-    str[2] = 'G';
-    str[3] = '0' + sig / 10;
-    str[4] = '0' + sig % 10;
-    str[5] = 0;
-
-    return str;
-}
+void get_pending_signals(struct shim_thread* thread, __sigset_t* set);
 
 struct shim_thread;
 
@@ -132,8 +106,7 @@ int init_signal (void);
 void __store_context (shim_tcb_t * tcb, PAL_CONTEXT * pal_context,
                       struct shim_signal * signal);
 
-// Need to hold thread->lock when calling this function
-void append_signal(struct shim_thread* thread, int sig, siginfo_t* info, bool need_interrupt);
+int append_signal(struct shim_thread* thread, siginfo_t* info);
 
 void deliver_signal(siginfo_t* info, PAL_CONTEXT* context);
 
@@ -145,7 +118,5 @@ int do_kill_thread (IDTYPE sender, IDTYPE tgid, IDTYPE tid, int sig,
                     bool use_ipc);
 int do_kill_proc (IDTYPE sender, IDTYPE tgid, int sig, bool use_ipc);
 int do_kill_pgroup (IDTYPE sender, IDTYPE pgid, int sig, bool use_ipc);
-
-int kill_all_threads (struct shim_thread * cur, IDTYPE sender, int sig);
 
 #endif /* _SHIM_SIGNAL_H_ */

commit de6974a91bf8df22537d1b74dc3d1cf7b29a72ae

[LibOS] Correctly propagate exit code from threads
Currently, a thread calling exit_group() will cause all other
threads to also exit, but the exit code for these threads will be
0. As a result, another process calling wait() might receive 0
instead of the right exit code.

Instead, make sure that all other threads exit with the same
exit_code and term_signal.

It looks like this regression was introduced in
50ca6c4a2284c9d06b48b6b3e8c2ef9eb10f0f90. I noticed it because
LTP test accept02 incorrectly returned exit code 0 before failing
(see https://github.com/oscarlab/graphene/issues/1937).

The previous commit strengthens a test (exit_group) that triggers
the described scenario.

LibOS/shim/include/shim_thread.h
@@ -98,6 +98,7 @@ struct shim_thread {
     int term_signal; // Store the terminating signal, if any; needed for
                      // wait() and friends
     bool is_alive;
+    /* Trigger thread exit. Set together with exit_code and term_signal. */
     bool time_to_die;
 
     PAL_HANDLE child_exit_event;
@@ -299,7 +300,7 @@ static inline void set_handle_map(struct shim_thread* thread, struct shim_handle
 }
 
 int thread_destroy(struct shim_thread* self, bool send_ipc);
-bool kill_other_threads(void);
+bool kill_other_threads(int error_code, int term_signal);
 noreturn void thread_exit(int error_code, int term_signal);
 noreturn void process_exit(int error_code, int term_signal);
 

LibOS/shim/src/bookkeep/shim_signal.c
@@ -762,7 +762,8 @@ void __handle_signals(shim_tcb_t* tcb) {
     }
 
     if (thread->time_to_die) {
-        thread_exit(/*error_code=*/0, /*term_signal=*/0);
+        debug("thread exit triggered from another thread\n");
+        thread_exit(-thread->exit_code, thread->term_signal);
     }
 
     while (__atomic_load_n(&thread->pending_signals, __ATOMIC_ACQUIRE)

LibOS/shim/src/sys/shim_exec.c
@@ -353,7 +353,7 @@ reopen:
         /* Just exit current thread. */
         thread_exit(/*error_code=*/0, /*term_signal=*/0);
     }
-    bool threads_killed = kill_other_threads();
+    bool threads_killed = kill_other_threads(/*error_code=*/0, /*term_signal=*/0);
 
     /* All other threads are dead. Restoring initial value in case we stay inside same process
      * instance and call execve again. */

LibOS/shim/src/sys/shim_exit.c
@@ -15,6 +15,12 @@
 #include "shim_thread.h"
 #include "shim_utils.h"
 
+struct thread_exit_params {
+    struct shim_thread* thread;
+    int exit_code;
+    int term_signal;
+};
+
 int thread_destroy(struct shim_thread* thread, bool send_ipc) {
     bool sent_exit_msg = false;
 
@@ -150,7 +156,9 @@ noreturn void thread_exit(int error_code, int term_signal) {
 }
 
 static int mark_thread_to_die(struct shim_thread* thread, void* arg) {
-    if (thread == (struct shim_thread*)arg) {
+    struct thread_exit_params* params = arg;
+
+    if (thread == params->thread) {
         return 0;
     }
 
@@ -161,6 +169,8 @@ static int mark_thread_to_die(struct shim_thread* thread, void* arg) {
         need_wakeup = true;
     }
     thread->time_to_die = true;
+    thread->exit_code = params->exit_code;
+    thread->term_signal = params->term_signal;
     unlock(&thread->lock);
 
     /* Now let's kick `thread`, so that it notices (in `__handle_signals`) the flag `time_to_die`
@@ -172,11 +182,17 @@ static int mark_thread_to_die(struct shim_thread* thread, void* arg) {
     return 1;
 }
 
-bool kill_other_threads(void) {
+bool kill_other_threads(int error_code, int term_signal) {
+    struct thread_exit_params params = {
+        .thread = get_cur_thread(),
+        .exit_code = -error_code,
+        .term_signal = term_signal,
+    };
+
     bool killed = false;
     /* Tell other threads to exit. Since `mark_thread_to_die` never returns an error, this call
      * cannot fail. */
-    if (walk_thread_list(mark_thread_to_die, get_cur_thread(), /*one_shot=*/false) != -ESRCH) {
+    if (walk_thread_list(mark_thread_to_die, &params, /*one_shot=*/false) != -ESRCH) {
         killed = true;
     }
     DkThreadYieldExecution();
@@ -185,7 +201,7 @@ bool kill_other_threads(void) {
     while (!check_last_thread()) {
         /* Tell other threads to exit again - the previous announcement could have been missed by
          * threads that were just being created. */
-        if (walk_thread_list(mark_thread_to_die, get_cur_thread(), /*one_shot=*/false) != -ESRCH) {
+        if (walk_thread_list(mark_thread_to_die, &params, /*one_shot=*/false) != -ESRCH) {
             killed = true;
         }
         DkThreadYieldExecution();
@@ -203,7 +219,7 @@ noreturn void process_exit(int error_code, int term_signal) {
         thread_exit(error_code, term_signal);
     }
 
-    (void)kill_other_threads();
+    (void)kill_other_threads(error_code, term_signal);
 
     /* Now quit our thread. Since we are the last one, this will exit the whole LibOS. */
     thread_exit(error_code, term_signal);

LibOS/shim/test/ltp/ltp.cfg
@@ -11,6 +11,10 @@
 [DEFAULT]
 timeout = 30
 
+# no setsockopt(SOL_IP)
+[accept02]
+skip = yes
+
 # utimensat
 [access01]
 skip = yes