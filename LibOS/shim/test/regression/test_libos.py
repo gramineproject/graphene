a5ebcb275c4358603d9992f3912f152320fcbb19
Paweł Marczewski <pawel@invisiblethingslab.com>
2020-11-04 16:52:24 +0100

    [LibOS] test/regression: Fix exit_group test
    The exit code of the test was a random number between 0 and 4,
    which gave no guarantee that the right code from exit() will
    actually reach the user.
    
    Instead of testing a race between threads, make it possible to
    call exit() in a chosen thread, and verify that it always works.
    
    In addition, propagate the exit code from a forked process. That
    use case is actually broken and will be fixed in next commit.

--- LibOS/shim/test/regression/test_libos.py
+++ LibOS/shim/test/regression/test_libos.py
@@ -186,10 +186,13 @@ class TC_01_Bootstrap(RegressionTestCase):
             self.run_binary(['exit'])
 
     def test_401_exit_group(self):
-        try:
-            self.run_binary(['exit_group'])
-        except subprocess.CalledProcessError as e:
-            self.assertTrue(1 <= e.returncode and e.returncode <= 4)
+        for thread_idx in range(4):
+            exit_code = 100 + thread_idx
+            try:
+                self.run_binary(['exit_group', str(thread_idx), str(exit_code)])
+                self.fail('expected to return {}'.format(exit_code))
+            except subprocess.CalledProcessError as e:
+                self.assertEqual(e.returncode, exit_code)
 
     def test_402_signalexit(self):
         with self.expect_returncode(134):
