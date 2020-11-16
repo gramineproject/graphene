4dd0b3e4f54bee236be0e7f3845fed55c066f4ed
Li, Xun <xun.li@intel.com>
2020-11-16 02:32:36 +0000

    [LibOS] fix sendfile to return EINVAL when out_fd has O_APPEND flag
    Current Graphene sendfile implementation doesn't check O_APPEND flag
    and tries to transfer data and transfers wrong data.
    This commit updates sendfile to align with manual
     "EINVAL out_fd has the O_APPEND flag set.  This is not currently supported by sendfile()."
    Also fixes not matched get_handle/put_handle.
    
    Signed-off-by: Li, Xun <xun.li@intel.com>

--- LibOS/shim/src/sys/shim_fs.c
+++ LibOS/shim/src/sys/shim_fs.c
@@ -606,13 +606,22 @@ out:
 
 ssize_t shim_do_sendfile(int ofd, int ifd, off_t* offset, size_t count) {
     struct shim_handle* hdli = get_fd_handle(ifd, NULL, NULL);
-    struct shim_handle* hdlo = get_fd_handle(ofd, NULL, NULL);
-
-    if (!hdli || !hdlo)
+    if (!hdli)
         return -EBADF;
 
+    struct shim_handle* hdlo = get_fd_handle(ofd, NULL, NULL);
+    if (!hdlo) {
+        put_handle(hdli);
+        return -EBADF;
+    }
+
+    int ret = -EINVAL;
+    /* EINVAL out_fd has the O_APPEND flag set.  This is not currently supported by sendfile(). */
+    if (hdlo->flags & O_APPEND)
+        goto out;
+
     off_t old_offset = 0;
-    int ret = -EACCES;
+    ret = -EACCES;
 
     if (offset) {
         if (!hdli->fs || !hdli->fs->fs_ops || !hdli->fs->fs_ops->seek)
