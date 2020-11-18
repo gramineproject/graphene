9a0d1fca93d7bcbea854cf2d9e4906f5f349cc62
Paweł Marczewski <pawel@invisiblethingslab.com>
2020-11-18 11:59:53 +0100

    [Pal,LibOS] Introduce human-readable PERM_* macros
    Instead of using S_I* flags, or hardcoded octal literals, use
    helpers such as PERM_rwxrwxr_x. These are proposed in a Linux patch
    by Inigo Molnar: https://lwn.net/Articles/696231/

--- Pal/include/lib/stat.h
+++ Pal/include/lib/stat.h
@@ -51,4 +51,37 @@
 
 #endif /* S_IFREG */
 
+/*
+ * We also define human-readable macros for common file permissions.
+ * Inspired by Linux patch by Inigo Molnar (https://lwn.net/Articles/696231/).
+ */
+
+#define PERM_r________	0400
+#define PERM_r__r_____	0440
+#define PERM_r__r__r__	0444
+
+#define PERM_rw_______	0600
+#define PERM_rw_r_____	0640
+#define PERM_rw_r__r__	0644
+#define PERM_rw_rw_r__	0664
+#define PERM_rw_rw_rw_	0666
+
+#define PERM__w_______	0200
+#define PERM__w__w____	0220
+#define PERM__w__w__w_	0222
+
+#define PERM_r_x______	0500
+#define PERM_r_xr_x___	0550
+#define PERM_r_xr_xr_x	0555
+
+#define PERM_rwx______	0700
+#define PERM_rwxr_x___	0750
+#define PERM_rwxr_xr_x	0755
+#define PERM_rwxrwxr_x	0775
+#define PERM_rwxrwxrwx	0777
+
+#define PERM__wx______	0300
+#define PERM__wx_wx___	0330
+#define PERM__wx_wx_wx	0333
+
 #endif /* STAT_H */
