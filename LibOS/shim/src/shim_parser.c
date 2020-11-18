756a2e41ce794699ad5d9486410e7bfe8b87066f
Paweł Marczewski <pawel@invisiblethingslab.com>
2020-11-18 13:30:13 +0100

    [LibOS] Use debug_log_level
    For now, make all debug() calls output PAL_LOG_INFO, except for
    syscall parser, which outputs PAL_LOG_TRACE.

--- LibOS/shim/src/shim_parser.c
+++ LibOS/shim/src/shim_parser.c
@@ -553,7 +553,7 @@ static inline void skip_syscall_args(va_list* ap) {
 }
 
 void parse_syscall_before(int sysno, const char* name, int nr, ...) {
-    if (!g_debug_log_enabled)
+    if (g_debug_log_level < PAL_LOG_TRACE)
         return;
 
     struct parser_table* parser = &syscall_parser_table[sysno];
@@ -589,7 +589,7 @@ dotdotdot:
 }
 
 void parse_syscall_after(int sysno, const char* name, int nr, ...) {
-    if (!g_debug_log_enabled)
+    if (g_debug_log_level < PAL_LOG_TRACE)
         return;
 
     struct parser_table* parser = &syscall_parser_table[sysno];
