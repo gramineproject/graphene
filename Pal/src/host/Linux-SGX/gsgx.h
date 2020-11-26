a7b6bff274c8ae955e9a14fcc934fac490ed5e34
Wojtek Porczyk <woju@invisiblethingslab.com>
2020-11-26 21:15:44 +0100

    [Pal] Linux-SGX: relicence to LGPL-3.0-or-later
    This was kindly approved by Dmitrii Kuvaiskii:
    https://github.com/oscarlab/graphene/pull/1997#pullrequestreview-539424903

--- Pal/src/host/Linux-SGX/gsgx.h
+++ Pal/src/host/Linux-SGX/gsgx.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
 /* (C) Copyright 2020 Intel Corporation
  *                    Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
  */
