68da8be26ceff5f6f1848bed13e108661bb978c8
Stefan Berger <stefanb@us.ibm.com>
2021-01-07 14:15:33 -0500

    [PAL] Consolidate PAL_TCB_LINUX init in pal_tcb_linux_init

--- Pal/src/host/Linux/db_threading.c
+++ Pal/src/host/Linux/db_threading.c
@@ -160,11 +160,7 @@ int _DkThreadCreate(PAL_HANDLE* handle, int (*callback)(void*), const void* para
 
     // Initialize TCB at the top of the alternative stack.
     PAL_TCB_LINUX* tcb = child_stack + ALT_STACK_SIZE - sizeof(PAL_TCB_LINUX);
-    tcb->common.self = &tcb->common;
-    tcb->handle      = hdl;
-    tcb->alt_stack   = child_stack; // Stack bottom
-    tcb->callback    = callback;
-    tcb->param       = (void*)param;
+    pal_tcb_linux_init(tcb, hdl, child_stack, callback, (void *)param);
 
     /* align child_stack to 16 */
     child_stack = ALIGN_DOWN_PTR(child_stack, 16);
