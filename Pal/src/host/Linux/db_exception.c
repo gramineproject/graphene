cfa829e9326fb11ec79551611719f3337bc0e050
borysp <borysp@invisiblethingslab.com>
2021-01-13 23:22:59 +0100

    [Pal] Reap zombie processes
    Previously all Linux Pals set `SIGCHILD` signal disposition to `SIG_DFL`
    and did not wait for child processes at all. This commit adds zombie
    reaping by setting disposition to `SIG_IGN`.

--- Pal/src/host/Linux/db_exception.c
+++ Pal/src/host/Linux/db_exception.c
@@ -218,11 +218,7 @@ void signal_setup(void) {
     if (ret < 0)
         goto err;
 
-    /* Even though SIG_DFL defaults to "ignore", this is not the same as SIG_IGN; man waitpid says:
-     * "...if the disposition of SIGCHLD is set to SIG_IGN ..., then children that terminate do not
-     * become zombies". In other words, if we would set_signal_handler(SIGCHLD, SIG_IGN) here,
-     * children would not become zombies and would die before the parent checks their status. */
-    ret = set_signal_handler(SIGCHLD, SIG_DFL);
+    ret = set_signal_handler(SIGCHLD, SIG_IGN);
     if (ret < 0)
         goto err;
 
