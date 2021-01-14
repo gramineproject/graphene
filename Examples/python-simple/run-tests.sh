7392b560a696101de283e3a5d3165d881904961d
Wojtek Porczyk <woju@invisiblethingslab.com>
2021-01-14 20:27:53 +0100

    [CI] Fix Examples/python-*

--- Examples/python-simple/run-tests.sh
+++ Examples/python-simple/run-tests.sh
@@ -4,23 +4,23 @@ set -e
 
 # === hellworld ===
 echo -e "\n\nRunning helloworld.py:"
-./pal_loader ./python scripts/helloworld.py > OUTPUT
+$GRAPHENE ./python scripts/helloworld.py > OUTPUT
 grep -q "Hello World" OUTPUT && echo "[ Success 1/3 ]"
 rm OUTPUT
 
 # === fibonacci ===
 echo -e "\n\nRunning fibonacci.py:"
-./pal_loader ./python scripts/fibonacci.py > OUTPUT
+$GRAPHENE ./python scripts/fibonacci.py > OUTPUT
 grep -q "fib2              55" OUTPUT && echo "[ Success 2/3 ]"
 rm OUTPUT
 
 # === web server and client (on port 8005) ===
 echo -e "\n\nRunning HTTP server dummy-web-server.py in the background:"
-./pal_loader ./python scripts/dummy-web-server.py 8005 & echo $! > server.PID
+$GRAPHENE ./python scripts/dummy-web-server.py 8005 & echo $! > server.PID
 sleep 30  # Graphene-SGX takes a lot of time to initialize
 
 echo -e "\n\nRunning HTTP client test-http.py:"
-./pal_loader ./python scripts/test-http.py localhost 8005 > OUTPUT1
+$GRAPHENE ./python scripts/test-http.py localhost 8005 > OUTPUT1
 wget -q http://localhost:8005/ -O OUTPUT2
 echo >> OUTPUT2  # include newline since wget doesn't add it
 # check if all lines from OUTPUT2 are included in OUTPUT1
