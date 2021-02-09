9e42388f5a98aec87afb11ecd431c281f4f90fc3
borysp <borysp@invisiblethingslab.com>
2021-02-09 02:54:58 +0100

    [PAL] Remove empty `ENTER_PAL_CALL`/`LEAVE_PAL_CALL` macros
    These macros were empty and not used anymore. Additionally there was
    a bug: `LEAVE_PAL_CALL` actually did not perform `return` and the
    execution continued after it.
    
    Signed-off-by: borysp <borysp@invisiblethingslab.com>

--- Pal/src/db_events.c
+++ Pal/src/db_events.c
@@ -12,8 +12,6 @@
 #include "pal_internal.h"
 
 PAL_HANDLE DkNotificationEventCreate(PAL_BOL initialState) {
-    ENTER_PAL_CALL(DkNotificationEventCreate);
-
     PAL_HANDLE handle = NULL;
     int ret = _DkEventCreate(&handle, initialState, true);
 
@@ -22,12 +20,10 @@ PAL_HANDLE DkNotificationEventCreate(PAL_BOL initialState) {
         handle = NULL;
     }
 
-    LEAVE_PAL_CALL_RETURN(handle);
+    return handle;
 }
 
 PAL_HANDLE DkSynchronizationEventCreate(PAL_BOL initialState) {
-    ENTER_PAL_CALL(DkSynchronizationEventCreate);
-
     PAL_HANDLE handle = NULL;
     int ret = _DkEventCreate(&handle, initialState, false);
 
@@ -36,39 +32,31 @@ PAL_HANDLE DkSynchronizationEventCreate(PAL_BOL initialState) {
         handle = NULL;
     }
 
-    LEAVE_PAL_CALL_RETURN(handle);
+    return handle;
 }
 
 /* DkEventDestroy deprecated, replaced by DkObjectClose */
 
 void DkEventSet(PAL_HANDLE handle) {
-    ENTER_PAL_CALL(DkEventSet);
-
     if (!handle || !IS_HANDLE_TYPE(handle, event)) {
         _DkRaiseFailure(PAL_ERROR_INVAL);
-        LEAVE_PAL_CALL();
+        return;
     }
 
     int ret = _DkEventSet(handle, -1);
 
     if (ret < 0)
         _DkRaiseFailure(-ret);
-
-    LEAVE_PAL_CALL();
 }
 
 void DkEventClear(PAL_HANDLE handle) {
-    ENTER_PAL_CALL(DkEventClear);
-
     if (!handle || !IS_HANDLE_TYPE(handle, event)) {
         _DkRaiseFailure(PAL_ERROR_INVAL);
-        LEAVE_PAL_CALL();
+        return;
     }
 
     int ret = _DkEventClear(handle);
 
     if (ret < 0)
         _DkRaiseFailure(-ret);
-
-    LEAVE_PAL_CALL();
 }
--- Pal/src/db_object.c
+++ Pal/src/db_object.c
@@ -39,62 +39,54 @@ int _DkObjectClose(PAL_HANDLE objectHandle) {
 
 /* PAL call DkObjectClose: Close the given object handle. */
 void DkObjectClose(PAL_HANDLE objectHandle) {
-    ENTER_PAL_CALL(DkObjectClose);
-
     if (!objectHandle) {
         _DkRaiseFailure(PAL_ERROR_INVAL);
-        LEAVE_PAL_CALL();
+        return;
     }
 
     int ret = _DkObjectClose(objectHandle);
     if (ret < 0)
         _DkRaiseFailure(-ret);
-
-    LEAVE_PAL_CALL();
 }
 
 /* Wait on a synchronization handle and return true if this handle's event was triggered,
  * otherwise return false and additionally raise failure. */
 PAL_BOL DkSynchronizationObjectWait(PAL_HANDLE handle, PAL_NUM timeout_us) {
-    ENTER_PAL_CALL(DkSynchronizationObjectWait);
-
     if (!handle) {
         _DkRaiseFailure(PAL_ERROR_INVAL);
-        LEAVE_PAL_CALL_RETURN(PAL_FALSE);
+        return PAL_FALSE;
     }
 
     int ret = _DkSynchronizationObjectWait(handle, timeout_us);
     if (ret < 0) {
         _DkRaiseFailure(-ret);
-        LEAVE_PAL_CALL_RETURN(PAL_FALSE);
+        return PAL_FALSE;
     }
 
-    LEAVE_PAL_CALL_RETURN(PAL_TRUE);
+    return PAL_TRUE;
 }
 
 /* Wait for user-specified events of handles in the handle array. The wait can be timed out, unless
  * NO_TIMEOUT is given in the timeout_us argument. Returns PAL_TRUE if waiting was successful. */
 PAL_BOL DkStreamsWaitEvents(PAL_NUM count, PAL_HANDLE* handle_array, PAL_FLG* events,
                             PAL_FLG* ret_events, PAL_NUM timeout_us) {
-    ENTER_PAL_CALL(DkStreamsWaitEvents);
-
     if (!count || !handle_array || !events || !ret_events) {
         _DkRaiseFailure(PAL_ERROR_INVAL);
-        LEAVE_PAL_CALL_RETURN(PAL_FALSE);
+        return PAL_FALSE;
     }
 
     for (PAL_NUM i = 0; i < count; i++) {
         if (UNKNOWN_HANDLE(handle_array[i])) {
             _DkRaiseFailure(PAL_ERROR_INVAL);
-            LEAVE_PAL_CALL_RETURN(PAL_FALSE);
+            return PAL_FALSE;
         }
     }
 
     int ret = _DkStreamsWaitEvents(count, handle_array, events, ret_events, timeout_us);
     if (ret < 0) {
         _DkRaiseFailure(-ret);
-        LEAVE_PAL_CALL_RETURN(PAL_FALSE);
+        return PAL_FALSE;
     }
 
-    LEAVE_PAL_CALL_RETURN(PAL_TRUE);
+    return PAL_TRUE;
 }
