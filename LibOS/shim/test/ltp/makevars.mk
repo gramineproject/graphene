19acfb64b9a0b875816017b031c19e36ef4257b3
Jinen Gandhi <jinen.gandhi@intel.com>
2021-02-18 00:08:30 -0600

    [CI, LTP] Add MAP_PRIVATE patch to LTP sources and enable LTP in SGX CI
    Signed-off-by: Jinen Gandhi <jinen.gandhi@intel.com>

--- LibOS/shim/test/ltp/makevars.mk
+++ LibOS/shim/test/ltp/makevars.mk
@@ -1,9 +1,14 @@
 # Everything here must be absolute because other Makefiles assume this.
 ROOTDIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-
 SRCDIR = $(ROOTDIR)/src
 BUILDDIR = $(ROOTDIR)/build
 INSTALLDIR = $(ROOTDIR)/install
+LTPROOT = install
+RUNLTPOPTS =
+ifeq ($(SGX),1)
+	BUILDDIR:=$(BUILDDIR)-sgx
+	INSTALLDIR:=$(INSTALLDIR)-sgx
+	LTPROOT = install-sgx
+endif
 TESTCASEDIR = $(INSTALLDIR)/testcases/bin
 LTPSCENARIO = $(INSTALLDIR)/runtest/syscalls
-RUNLTPOPTS =
