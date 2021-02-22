8b0bdcc33d82bd8428adc8b1f8c6db08b65eddb7
Sankaranarayanan Venkatasubramanian <svenkat9>
2021-02-22 00:30:52 -0800

    [LibOS] procfs: Add support for /proc/self/cmdline
    Signed-off-by: Sankaranarayanan Venkatasubramanian <sankara.narayanan.v@intel.com>

--- LibOS/shim/src/fs/proc/thread.c
+++ LibOS/shim/src/fs/proc/thread.c
@@ -607,8 +607,8 @@ static int proc_thread_cmdline_open(struct shim_handle* hdl, const char* name, i
     if (ret < 0)
         return ret;
 
-    size_t buffer_size = g_process.cmdline_length;
-    buffer = calloc(buffer_size, sizeof(char));
+    size_t buffer_size = g_process.cmdline_size;
+    buffer = malloc(buffer_size);
     if (!buffer) {
         return -ENOMEM;
     }
@@ -627,9 +627,6 @@ static int proc_thread_cmdline_open(struct shim_handle* hdl, const char* name, i
     hdl->flags         = flags & ~O_RDONLY;
     hdl->acc_mode      = MAY_READ;
     hdl->info.str.data = data;
-
-    // Don't really need to do any work here, but keeping as a placeholder,
-    // just in case.
 
     return 0;
 }
