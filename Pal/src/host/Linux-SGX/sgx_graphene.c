53d4f3f23771186fd7192d30de6e4ed3afbb5562
Michał Kowalczyk <mkow@invisiblethingslab.com>
2021-02-23 17:18:35 +0100

    fixup! [Pal] Clean up printf-related code a bit
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- Pal/src/host/Linux-SGX/sgx_graphene.c
+++ Pal/src/host/Linux-SGX/sgx_graphene.c
@@ -34,14 +34,14 @@ static int fputch(void* f, int ch, void* _buf) {
 }
 
 static int vfdprintf(int fd, const char* fmt, va_list ap) {
-    struct printbuf b;
+    struct printbuf buf;
 
-    b.idx = 0;
-    b.cnt = 0;
-    vfprintfmt(fputch, NULL, &b, fmt, ap);
-    INLINE_SYSCALL(write, 3, fd, b.buf, b.idx);
+    buf.idx = 0;
+    buf.cnt = 0;
+    vfprintfmt(fputch, NULL, &buf, fmt, ap);
+    INLINE_SYSCALL(write, 3, fd, buf.buf, buf.idx);
 
-    return b.cnt;
+    return buf.cnt;
 }
 
 int pal_printf(const char* fmt, ...) {
