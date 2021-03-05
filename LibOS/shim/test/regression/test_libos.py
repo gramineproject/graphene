9825c84b58aca8f67bf4c1acea9aa176719825c9
Vijay Dhanraj <vijay.dhanraj@intel.com>
2021-03-05 07:24:01 -0800

    [LibOS] regression: Skip mmap test case when SGX=1
    This test case passes without EDMM as all the enclave pages
    are already EADDed but with EDMM, a SIGBUS is generated since
    pages are only added when needed. Since SIGBUS isn't
    implemented correctly for lack of memory protection, skipping
    the mmap test case. Also, updated the mmap test case to remove
    default signal handler.
    
    Signed-off-by: Vijay Dhanraj <vijay.dhanraj@intel.com>

--- LibOS/shim/test/regression/test_libos.py
+++ LibOS/shim/test/regression/test_libos.py
@@ -458,14 +458,6 @@ class TC_30_Syscall(RegressionTestCase):
         self.assertIn('mmap test 3 passed', stdout)
         self.assertIn('mmap test 4 passed', stdout)
 
-        # "test 5" and "test 8" are checked below, in test_051_mmap_sgx
-
-    @unittest.skipIf(HAS_SGX,
-        'On SGX, SIGBUS isn\'t always implemented correctly, for lack '
-        'of memory protection. For now, some of these cases won\'t work.')
-    def test_051_mmap_sgx(self):
-        stdout, _ = self.run_binary(['mmap_file'], timeout=60)
-
         # SIGBUS test
         self.assertIn('mmap test 5 passed', stdout)
         self.assertIn('mmap test 8 passed', stdout)
