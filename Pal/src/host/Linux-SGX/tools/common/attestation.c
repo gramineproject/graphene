1ba666b111d30273a996b6b581c6792172a4995a
Wojtek Porczyk <woju@invisiblethingslab.com>
2021-03-19 21:44:30 +0100

    [Pal/Linux-SGX] attestation: when overridden, allow other unpatched systems
    GROUP_OUT_OF_DATE is only one of possible answers, which IAS API [1]
    might return in case of some low-level configuration problems.
    
    This change does not actually cause any more exposure, because if the
    system is both _OUT_OF_DATE and there is CONFIGURATION_ and/or
    SW_HARDENING_NEEDED, IAS returns GROUP_OUT_OF_DATE [2]. Therefore, if
    GROUP_OUT_OF_DATE is accepted, attacker facing CONFIGURATION_NEEDED
    might just downgrade BIOS, microcode and/or PSW to have
    GROUP_OUT_OF_DATE instead.
    
    [1] https://api.trustedservices.intel.com/documents/sgx-attestation-api-spec.pdf
    [2] https://cdrdv2.intel.com/v1/dl/getContent/619320
    
    Signed-off-by: Wojtek Porczyk <woju@invisiblethingslab.com>

--- Pal/src/host/Linux-SGX/tools/common/attestation.c
+++ Pal/src/host/Linux-SGX/tools/common/attestation.c
@@ -217,9 +217,14 @@ int verify_ias_report_extract_quote(const uint8_t* ias_report, size_t ias_report
     if (strcmp("OK", node->valuestring) == 0) {
         ret = 0;
         INFO("IAS report: quote status OK\n");
-    } else if (allow_outdated_tcb && strcmp("GROUP_OUT_OF_DATE", node->valuestring) == 0) {
+    } else if (allow_outdated_tcb && (
+               strcmp("GROUP_OUT_OF_DATE", node->valuestring) == 0
+            || strcmp("CONFIGURATION_NEEDED", node->valuestring) == 0
+            || strcmp("SW_HARDENING_NEEDED", node->valuestring) == 0
+            || strcmp("CONFIGURATION_AND_SW_HARDENING_NEEDED", node->valuestring) == 0
+            )) {
         ret = 0;
-        INFO("IAS report: allowing quote status GROUP_OUT_OF_DATE\n");
+        INFO("IAS report: allowing quote status %s\n", node->valuestring);
     }
 
     if (ret != 0) {
