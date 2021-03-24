a62f4cc4097ec9238214e97ef7ce4669af7a6bdf
Paweł Marczewski <pawel@invisiblethingslab.com>
2021-03-24 18:02:42 +0100

    [Examples/python-simple] Don't suppress output on test crash
    Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

--- Examples/python-simple/run-tests.sh
+++ Examples/python-simple/run-tests.sh
@@ -2,16 +2,28 @@
 
 set -e
 
+failures=0
+
 # === hellworld ===
 echo -e "\n\nRunning helloworld.py:"
 ./pal_loader ./python scripts/helloworld.py > OUTPUT
-grep -q "Hello World" OUTPUT && echo "[ Success 1/3 ]"
+if grep -q "Hello World" OUTPUT; then
+    echo "[ Success 1/3 ]"
+else
+    echo "[ Failure 1/3 ]"
+    failures=$((failures+1))
+fi
 rm OUTPUT
 
 # === fibonacci ===
 echo -e "\n\nRunning fibonacci.py:"
 ./pal_loader ./python scripts/fibonacci.py > OUTPUT
-grep -q "fib2              55" OUTPUT && echo "[ Success 2/3 ]"
+if grep -q "fib2              55" OUTPUT; then
+    echo "[ Success 2/3 ]"
+else
+    echo "[ Failure 2/3 ]"
+    failures=$((failures+1))
+fi
 rm OUTPUT
 
 # === web server and client (on port 8005) ===
@@ -27,6 +39,13 @@ echo >> OUTPUT2  # include newline since wget doesn't add it
 # check if all lines from OUTPUT2 are included in OUTPUT1
 # TODO: simplify after fixing Graphene logging subsystem, which currently mixes its output with the
 # application output.
-diff OUTPUT1 OUTPUT2 | grep -q '^>' || echo "[ Success 3/3 ]"
+if ! diff OUTPUT1 OUTPUT2 | grep -q '^>'; then
+    echo "[ Success 3/3 ]"
+else
+    echo "[ Failure 3/3 ]"
+    failures=$((failures+1))
+fi
 kill "$(cat server.PID)"
 rm -f OUTPUT1 OUTPUT2 server.PID
+
+exit "$failures"
