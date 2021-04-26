c5220b0b993504deb01931cb3961fbad34231c10
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2021-04-26 09:48:43 -0700

    [Examples] python-simple: use `127.0.0.1` instead of `locahost` in test scripts
    For some reason (needs more analysis and a proper fix), the `urllib3`
    package used in Python 3.8 (which is the default on Ubuntu 20.04) and
    backed by Glibc 2.33 tries to perform `socket(AF_INET6)` plus
    `bind(<sock>, ::1:0)` (notice the zero port) plus `connect(<sock>)`.
    This results in EADDRNOTAVAIL error when Graphene tries to perform
    host-level `bind()`. This doesn't happen on Python 3.6 (default on
    Ubuntu 18.04). This also doesn't happen on Glibc 2.31.
    
    For now, we replace `localhost` (which needs to be resolved with the
    help of Glibc's getaddrinfo) with IPv4 `127.0.0.1`.
    
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- Examples/python-simple/run-tests.sh
+++ Examples/python-simple/run-tests.sh
@@ -21,8 +21,8 @@ echo -e "\n\nRunning HTTP server dummy-web-server.py in the background:"
 ../../Scripts/wait_for_server 60 127.0.0.1 8005
 
 echo -e "\n\nRunning HTTP client test-http.py:"
-./pal_loader ./python scripts/test-http.py localhost 8005 > OUTPUT1
-wget -q http://localhost:8005/ -O OUTPUT2
+./pal_loader ./python scripts/test-http.py 127.0.0.1 8005 > OUTPUT1
+wget -q http://127.0.0.1:8005/ -O OUTPUT2
 echo >> OUTPUT2  # include newline since wget doesn't add it
 # check if all lines from OUTPUT2 are included in OUTPUT1
 # TODO: simplify after fixing Graphene logging subsystem, which currently mixes its output with the
