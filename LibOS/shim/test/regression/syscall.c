36963766f72a8723d3778274355d9cecd8a026f5
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2021-05-18 23:06:51 -0700

    fixup! [LibOS,Pal] Move common lib (`graphene-lib.a`) under `graphene-root/Common`
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- LibOS/shim/test/regression/syscall.c
+++ LibOS/shim/test/regression/syscall.c
@@ -1,9 +1,14 @@
 #define _GNU_SOURCE
+#include <err.h>
+#include <stdlib.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
 int main(int argc, char** argv) {
     const char buf[] = "Hello world\n";
-    syscall(__NR_write, 1, buf, sizeof(buf) - 1);
+    long ret = syscall(__NR_write, 1, buf, sizeof(buf) - 1);
+    if (ret < 0)
+        err(EXIT_FAILURE, "write syscall");
+
     return 0;
 }
