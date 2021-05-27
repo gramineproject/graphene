335024117a4f34f5798c61328619f9ab12aba9da
Wojtek Porczyk <woju@invisiblethingslab.com>
2021-05-27 16:13:38 +0200

    fixup! [python] Remove support for running from repo
    Signed-off-by: Wojtek Porczyk <woju@invisiblethingslab.com>

--- python/graphenelibos/__init__.py
+++ python/graphenelibos/__init__.py
@@ -4,3 +4,9 @@ __version__ = '@VERSION@'
 
 _CONFIG_PKGLIBDIR = '@PKGLIBDIR@'
 _CONFIG_LIBDIR = '@LIBDIR@'
+
+if __version__.startswith('@'):
+    raise RuntimeError(
+        'You are attempting to run the tools from repo, without installing. '
+        'Please install graphene before running Python tools. See '
+        'https://graphene.readthedocs.io/en/latest/building.html.')
