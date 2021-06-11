4b3a22e92d689e68ba3c6336fa0dae7e974c27c9
Wojtek Porczyk <woju@invisiblethingslab.com>
2021-06-11 19:26:01 +0200

    [common] Add tolower() to ctype.c
    Previously this was a static in inet_pton.c, with wrong signature.
    
    Signed-off-by: Wojtek Porczyk <woju@invisiblethingslab.com>

--- common/src/string/ctype.c
+++ common/src/string/ctype.c
@@ -7,6 +7,17 @@ int islower(int c) {
     return (unsigned)c - 'a' < 26;
 }
 
+int isupper(int c)
+{
+    return (unsigned)c - 'A' < 26;
+}
+
+int tolower(int c)
+{
+    if (isupper(c)) return c | 32;
+    return c;
+}
+
 int toupper(int c) {
     if (islower(c))
         return c & 0x5f;
