6bb149200e676c220606e482b0c12e2c5516ad80
Michał Kowalczyk <mkow@invisiblethingslab.com>
2021-06-15 00:19:30 +0200

    fixup! [Pal/Linux-SGX] Properly handle failed allocation in ocall_exit()
    Signed-off-by: Michał Kowalczyk <mkow@invisiblethingslab.com>

--- Pal/src/host/Linux-SGX/enclave_ocalls.c
+++ Pal/src/host/Linux-SGX/enclave_ocalls.c
@@ -145,9 +145,9 @@ noreturn void ocall_exit(int exitcode, int is_exitgroup) {
     sgx_prepare_ustack();
     ms = sgx_alloc_on_ustack_aligned(sizeof(*ms), alignof(*ms));
     if (!ms) {
-        // We can't really recover from here. Should be unreachable without the host doing malicious
-        // things.
-        while (1) {}
+        /* We can't really recover from here. Should be unreachable without the host doing malicious
+         * things. */
+        die_or_inf_loop();
     }
     WRITE_ONCE(ms->ms_exitcode, exitcode);
     WRITE_ONCE(ms->ms_is_exitgroup, is_exitgroup);
