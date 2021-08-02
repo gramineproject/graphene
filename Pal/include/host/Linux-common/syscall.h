41fbbdd5297055233fcd10544cac16587c9aa8cf
Borys Popławski <borysp@invisiblethingslab.com>
2021-08-02 21:03:50 +0200

    fixup! [Pal] Make "syscall" instruction appear only once in PAL binary
    Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

--- Pal/include/host/Linux-common/syscall.h
+++ Pal/include/host/Linux-common/syscall.h
@@ -8,9 +8,25 @@ long clone(int (*f)(void*), void* stack, int flags, void* arg, void* parent_tid,
            void* child_tid, void (*exit_func)(int));
 long vfork(void) __attribute__((returns_twice));
 
-#define DO_SYSCALL(name, args...) do_syscall(__NR_##name, ##args)
+#define _extend(x) ((unsigned long)(x))
 
-#define IS_PTR_ERR(val) ((unsigned long)(val) >= (unsigned long)-4095L)
+#define DO_SYSCALL_0(name) do_syscall(name)
+#define DO_SYSCALL_1(name, a1) do_syscall(name, _extend(a1))
+#define DO_SYSCALL_2(name, a1, a2) do_syscall(name, _extend(a1), _extend(a2))
+#define DO_SYSCALL_3(name, a1, a2, a3) do_syscall(name, _extend(a1), _extend(a2), _extend(a3))
+#define DO_SYSCALL_4(name, a1, a2, a3, a4) \
+    do_syscall(name, _extend(a1), _extend(a2), _extend(a3), _extend(a4))
+#define DO_SYSCALL_5(name, a1, a2, a3, a4, a5) \
+    do_syscall(name, _extend(a1), _extend(a2), _extend(a3), _extend(a4), _extend(a5))
+#define DO_SYSCALL_6(name, a1, a2, a3, a4, a5, a6) \
+    do_syscall(name, _extend(a1), _extend(a2), _extend(a3), _extend(a4), _extend(a5), _extend(a6))
+
+#define PICK_SYSCALL_MACRO(name, _1, _2, _3, _4, _5, _6, NARGS, ...) DO_SYSCALL_##NARGS
+
+#define DO_SYSCALL(name, args...) \
+    PICK_SYSCALL_MACRO(name, ##args, 6, 5, 4, 3, 2, 1, 0)(__NR_##name, ##args)
+
+#define IS_PTR_ERR(val) ((unsigned long)(val) >= -4095UL)
 #define PTR_TO_ERR(val) ((long)val)
 #define IS_UNIX_ERR(val) ((val) >= -133 /* EHWPOISON */ && (val) <= -1 /* EPERM */)
 
