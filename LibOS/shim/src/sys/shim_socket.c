09864df70706a1bc1ad6ec12846d98043e000d93
Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>
2021-08-09 05:21:01 -0700

    fixup! [LibOS] Relax check on UNIX domain socket's `addrlen` in `bind`
    Signed-off-by: Dmitrii Kuvaiskii <dmitrii.kuvaiskii@intel.com>

--- LibOS/shim/src/sys/shim_socket.c
+++ LibOS/shim/src/sys/shim_socket.c
@@ -473,12 +473,22 @@ long shim_do_bind(int sockfd, struct sockaddr* addr, int _addrlen) {
         }
 
         struct sockaddr_un* saddr = (struct sockaddr_un*)addr;
-        char* spath               = saddr->sun_path;
-        struct shim_dentry* dent  = NULL;
-
-        if ((ret = path_lookupat(/*start=*/NULL, spath, LOOKUP_NO_FOLLOW | LOOKUP_CREATE, &dent)) < 0) {
+        if (saddr->sun_path[0] == '\0') {
+            /* currently do not support abstract UNIX domain sockets */
             goto out;
         }
+
+        size_t sun_path_size = addrlen - offsetof(struct sockaddr_un, sun_path);
+        if (strnlen(saddr->sun_path, sun_path_size) == sun_path_size) {
+            /* user-supplied UNIX domain name is not a NULL-terminating string */
+            goto out;
+        }
+
+        struct shim_dentry* dent = NULL;
+        ret = path_lookupat(/*start=*/NULL, saddr->sun_path, LOOKUP_NO_FOLLOW | LOOKUP_CREATE,
+                            &dent);
+        if (ret < 0)
+            goto out;
 
         if (!(dent->state & DENTRY_NEGATIVE)) {
             ret = -EADDRINUSE;
@@ -737,16 +747,30 @@ long shim_do_connect(int sockfd, struct sockaddr* addr, int _addrlen) {
     }
 
     if (sock->domain == AF_UNIX) {
-        if (addrlen != sizeof(struct sockaddr_un))
-            goto out;
-
-        struct sockaddr_un* saddr = (struct sockaddr_un*)addr;
-        char* spath               = saddr->sun_path;
-        struct shim_dentry* dent  = NULL;
-
-        if ((ret = path_lookupat(/*start=*/NULL, spath, LOOKUP_CREATE | LOOKUP_FOLLOW, &dent)) < 0) {
+        if (addrlen < offsetof(struct sockaddr_un, sun_path) + 1 ||
+                addrlen > sizeof(struct sockaddr_un)) {
+            /* address length of the UNIX domain socket is typically calculated as
+             * `offsetof(struct sockaddr_un, sun_path) + strlen(sun_path) + 1`, see `man unix` */
             goto out;
         }
+
+        struct sockaddr_un* saddr = (struct sockaddr_un*)addr;
+        if (saddr->sun_path[0] == '\0') {
+            /* currently do not support abstract UNIX domain sockets */
+            goto out;
+        }
+
+        size_t sun_path_size = addrlen - offsetof(struct sockaddr_un, sun_path);
+        if (strnlen(saddr->sun_path, sun_path_size) == sun_path_size) {
+            /* user-supplied UNIX domain name is not a NULL-terminating string */
+            goto out;
+        }
+
+        struct shim_dentry* dent = NULL;
+        ret = path_lookupat(/*start=*/NULL, saddr->sun_path, LOOKUP_NO_FOLLOW | LOOKUP_CREATE,
+                            &dent);
+        if (ret < 0)
+            goto out;
 
         if (!(dent->state & DENTRY_NEGATIVE) && dent->type != S_IFSOCK) {
             ret = -ECONNREFUSED;
