2bfc24943289fbd9bb76647ac45be76568110ffc
Paweł Marczewski <pawel@invisiblethingslab.com>
2021-08-18 18:03:16 +0200

    [Common] Rewrite standard functions to not invoke UB
    - `strtol` handles overflow as specified (i.e. returns
       LONG_MIN/LONG_MAX)
    - `atoi`/`atol` explicitly allow overflow (as they're not supposed to
       check for errors)
    - `strlen`/`strnlen` are replaced with a straightforward implementation
    
    Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

--- common/src/string/atoi.c
+++ common/src/string/atoi.c
@@ -25,9 +25,9 @@
 
 #include "api.h"
 
-long strtol(const char* s, char** endptr, int base) {
-    int neg  = 0;
-    long val = 0;
+static void begin_number(const char* str, int base, const char** out_s, int* out_base,
+                         int* out_sign) {
+    const char* s = str;
 
     // gobble initial whitespace
     while (*s == ' ' || *s == '\t') {
@@ -35,40 +35,73 @@ long strtol(const char* s, char** endptr, int base) {
     }
 
     // plus/minus sign
-    if (*s == '+')
+    int sign = 1;
+    if (*s == '+') {
         s++;
-    else if (*s == '-')
-        s++, neg = 1;
-
-    // hex or octal base prefix
-    if ((base == 0 || base == 16) && (s[0] == '0' && s[1] == 'x'))
-        s += 2, base = 16;
-    else if (base == 0 && s[0] == '0')
-        s++, base = 8;
-    else if (base == 0)
-        base = 10;
-
-    // digits
-    while (1) {
-        int dig;
-
-        if (*s >= '0' && *s <= '9')
-            dig = *s - '0';
-        else if (*s >= 'a' && *s <= 'z')
-            dig = *s - 'a' + 10;
-        else if (*s >= 'A' && *s <= 'Z')
-            dig = *s - 'A' + 10;
-        else
-            break;
-        if (dig >= base)
-            break;
-        s++, val = (val * base) + dig;
-        // we don't properly detect overflow!
+    } else if (*s == '-') {
+        s++;
+        sign = -1;
     }
 
-    if (endptr)
-        *endptr = (char*)s;
-    return (neg ? -val : val);
+    // hex or octal base prefix
+    if ((base == 0 || base == 16) && (s[0] == '0' && s[1] == 'x')) {
+        s += 2;
+        base = 16;
+    } else if (base == 0 && s[0] == '0') {
+        s++;
+        base = 8;
+    } else if (base == 0) {
+        base = 10;
+    }
+
+    *out_s = s;
+    *out_base = base;
+    *out_sign = sign;
+}
+
+static int parse_digit(char c, int base) {
+    int digit;
+
+    if (c >= '0' && c <= '9') {
+        digit = c - '0';
+    } else if (c >= 'a' && c <= 'z') {
+        digit = c - 'a' + 10;
+    } else if (c >= 'A' && c <= 'Z') {
+        digit = c - 'A' + 10;
+    } else {
+        return -1;
+    }
+    if (digit >= base)
+        return -1;
+    return digit;
+}
+
+long strtol(const char* str, char** out_end, int base) {
+    const char* s;
+    int sign;
+
+    begin_number(str, base, &s, &base, &sign);
+
+    long value = 0;
+    while (*s != '\0') {
+        int digit = parse_digit(*s, base);
+        if (digit == -1)
+            break;
+
+        if (__builtin_mul_overflow(value, base, &value)) {
+            return sign > 0 ? LONG_MAX : LONG_MIN;
+        }
+
+        if (__builtin_add_overflow(value, digit * sign, &value)) {
+            return sign > 0 ? LONG_MAX : LONG_MIN;
+        }
+
+        s++;
+    }
+
+    if (out_end)
+        *out_end = (char*)s;
+    return value;
 }
 
 int str_to_ulong(const char* str, unsigned int base, unsigned long* out_value,
@@ -79,17 +112,8 @@ int str_to_ulong(const char* str, unsigned int base, unsigned long* out_value,
     unsigned long value = 0;
     const char* s = str;
     while (*s != '\0') {
-        unsigned int digit;
-        if ('0' <= *s && *s <= '9') {
-            digit = *s - '0';
-        } else if ('a' <= *s && *s <= 'z') {
-            digit = *s - 'a' + 10;
-        } else if ('A' <= *s && *s <= 'Z') {
-            digit = *s - 'A' + 10;
-        } else {
-            break;
-        }
-        if (digit >= base)
+        int digit = parse_digit(*s, base);
+        if (digit == -1)
             break;
 
         if (__builtin_mul_overflow(value, base, &value))
@@ -118,14 +142,31 @@ long long strtoll(const char* s, char** endptr, int base) {
 #error "Unsupported architecture (only support data model LP64)"
 #endif
 
-/* Convert a string to an int.  */
-int atoi(const char* nptr) {
-    return (int)strtol(nptr, (char**)NULL, 10);
+/* Convert a string to an int (without error checking). */
+int atoi(const char* str) {
+    return (int)atol(str);
 }
 
-/* Convert a string to an long int.  */
-long int atol(const char* nptr) {
-    return strtol(nptr, (char**)NULL, 10);
+/* Convert a string to a long int (without error checking). */
+long int atol(const char* str) {
+    const char* s;
+    int sign;
+    int base;
+    begin_number(str, 10, &s, &base, &sign);
+    assert(base == 10);
+
+    long value = 0;
+    while (*s != '\0') {
+        int digit = parse_digit(*s, 10);
+        if (digit == -1)
+            break;
+
+        __builtin_mul_overflow(value, 10, &value);
+        __builtin_add_overflow(value, digit * sign, &value);
+
+        s++;
+    }
+    return value;
 }
 
 /* Parse a size (number with optional "G"/"M"/"K" suffix) into an unsigned long. Returns -1 if
