be8af622ba65013e9cc32fc0e326ee28992e92ae
Paweł Marczewski <pawel@invisiblethingslab.com>
2021-08-19 15:25:48 +0200

    Simplify strlen

--- common/src/string/strlen.c
+++ common/src/string/strlen.c
@@ -8,19 +8,15 @@
 /* Find the length of S, but scan at most MAXLEN characters.  If no
    '\0' terminator is found in that many characters, return MAXLEN.  */
 size_t strnlen(const char* str, size_t maxlen) {
-    size_t len = 0;
-    while (len < maxlen && *str != '\0') {
-        str++;
-        len++;
-    }
+    size_t len;
+    for (len = 0; len < maxlen && str[len] != '\0'; len++)
+        ;
     return len;
 }
 
 size_t strlen(const char* str) {
-    size_t len = 0;
-    while (*str != '\0') {
-        str++;
-        len++;
-    }
+    size_t len;
+    for (len = 0; str[len] != '\0'; len++)
+        ;
     return len;
 }
