7f3642ad685692ee346881382a4675d945a8e883
Paweł Marczewski <pawel@invisiblethingslab.com>
2021-08-24 10:28:34 +0200

    fixup! [Common] Rewrite standard functions to not invoke UB
    Signed-off-by: Paweł Marczewski <pawel@invisiblethingslab.com>

--- common/src/string/atoi.c
+++ common/src/string/atoi.c
@@ -1,23 +1,6 @@
-/* Copyright (C) 1991, 1997 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
-
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307 USA. */
-
 /* Copyright (C) 2021 Intel Corporation
  *                    Vijay Dhanraj <vijay.dhanraj@intel.com>
+ *                    Paweł Marczewski <pawel@invisiblethingslab.com>
  */
 
 #include <limits.h>
@@ -161,8 +144,8 @@ long int atol(const char* str) {
         if (digit == -1)
             break;
 
-        __builtin_mul_overflow(value, 10, &value);
-        __builtin_add_overflow(value, digit * sign, &value);
+        value *= 10;
+        value += digit * sign;
 
         s++;
     }
