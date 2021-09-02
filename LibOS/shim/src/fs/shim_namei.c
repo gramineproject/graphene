54e680cc5796ca99f0472463d830bce0f8065c2c
Sonali Saha <sonali.saha@intel.com>
2021-09-02 06:03:01 -0700

    fixup! [LibOS] Return -EISDIR on opening a directory with O_CREAT flag
    Signed-off-by: Sonali Saha <sonali.saha@intel.com>

--- LibOS/shim/src/fs/shim_namei.c
+++ LibOS/shim/src/fs/shim_namei.c
@@ -522,7 +522,9 @@ int open_namei(struct shim_handle* hdl, struct shim_dentry* start, const char* p
     assert(dent->state & DENTRY_VALID);
 
     if (dent->type == S_IFDIR) {
-        if (flags & O_WRONLY || flags & O_RDWR || flags & O_CREAT) {
+        if (flags & O_WRONLY || flags & O_RDWR
+                || ((flags & O_CREAT) && !(flags & O_DIRECTORY))
+                || ((flags & O_CREAT) && !(flags & O_EXCL))) {
             ret = -EISDIR;
             goto err;
         }
