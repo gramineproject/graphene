b32b66df40b4dbea2c1dcc5843712124d1117ca2
Borys Popławski <borysp@invisiblethingslab.com>
2021-09-03 01:36:16 +0200

    [LibOS] Correctly distinguish Linux access modes
    Linux access modes are values not bit masks.
    
    Signed-off-by: Borys Popławski <borysp@invisiblethingslab.com>

--- LibOS/shim/include/shim_flags_conv.h
+++ LibOS/shim/include/shim_flags_conv.h
@@ -1,6 +1,8 @@
 /* SPDX-License-Identifier: LGPL-3.0-or-later */
 /* Copyright (C) 2020 Invisible Things Lab
  *                    Michał Kowalczyk <mkow@invisiblethingslab.com>
+ * Copyright (C) 2021 Intel Corporation
+ *                    Borys Popławski <borysp@invisiblethingslab.com>
  */
 
 /*
@@ -18,6 +20,7 @@
 
 #include "assert.h"
 #include "pal.h"
+#include "shim_internal.h"
 
 static inline int LINUX_PROT_TO_PAL(int prot, int map_flags) {
     assert(WITHIN_MASK(prot, PROT_NONE | PROT_READ | PROT_WRITE | PROT_EXEC
@@ -29,10 +32,24 @@ static inline int LINUX_PROT_TO_PAL(int prot, int map_flags) {
 }
 
 static inline int LINUX_OPEN_FLAGS_TO_PAL_ACCESS(int access) {
-    return (access & O_RDONLY ? PAL_ACCESS_RDONLY : 0) |
-           (access & O_WRONLY ? PAL_ACCESS_WRONLY : 0) |
-           (access & O_RDWR   ? PAL_ACCESS_RDWR   : 0) |
-           (access & O_APPEND ? PAL_ACCESS_APPEND : 0);
+    int ret = 0;
+    switch (access & O_ACCMODE) {
+        case O_RDONLY:
+            ret = PAL_ACCESS_RDONLY;
+            break;
+        case O_WRONLY:
+            ret = PAL_ACCESS_WRONLY;
+            break;
+        case O_RDWR:
+            ret = PAL_ACCESS_RDWR;
+            break;
+        default:
+            BUG();
+    }
+    if (access & O_APPEND) {
+        ret |= PAL_ACCESS_APPEND;
+    }
+    return ret;
 }
 
 static inline int LINUX_OPEN_FLAGS_TO_PAL_CREATE(int flags) {
